1fa334c2608a9db8e4e1becaccce777122180fde
Nikolaus Wagner <e11722950@student.tuwien.ac.at>
2019-09-10 16:39:01 +0200

    style: Code cleanup of segmentation

--- segmentation/IncrementalSegmentation.cpp
+++ segmentation/IncrementalSegmentation.cpp
@@ -1,19 +1,13 @@
-//
-// Created by simon on 11/21/18.
-//
-
 #include "IncrementalSegmentation.h"
 
 using namespace std;
 using namespace cv;
 
-
-cv::Mat EdithSegmentation::generateNewLabels(cv::Mat *depth,
-        cv::Mat *normals,
-        cv::Mat *color,
-        cv::Mat *existingLabels) {
-    Mat placeholder(depth->cols,depth->rows,CV_32SC4);
-    placeholder = 100000;
-    return placeholder;
+cv::Mat EdithSegmentation::generateNewLabels(cv::Mat *depth, cv::Mat *normals,
+                                             cv::Mat *color, 
+                                             cv::Mat *existing_labels) {
+	Mat placeholder(depth->cols, depth->rows, CV_32SC4);
+	placeholder = 100000;
+	return placeholder;
 }
 
--- segmentation/IncrementalSegmentation.h
+++ segmentation/IncrementalSegmentation.h
@@ -1,41 +1,26 @@
-//
-// Created by simon on 11/21/18.
-//
-
-#ifndef SUPERMAPPING_INCREMENTALSEGMENTATION_H
-#define SUPERMAPPING_INCREMENTALSEGMENTATION_H
-
-
+#ifndef FILE_INCREMENTALSEGMENTATION_H
+#define FILE_INCREMENTALSEGMENTATION_H
 
 #include <opencv2/core.hpp>
 
-class IncrementalSegmentation{
+class IncrementalSegmentation {
 public:
 
-    virtual void initInThread() = 0;
-    virtual cv::Mat generateNewLabels(cv::Mat *depth,
-            cv::Mat *normals,
-            cv::Mat *color,
-            cv::Mat *existingLabels) = 0;
+	virtual void initInThread() = 0;
+	virtual cv::Mat generateNewLabels(cv::Mat *depth, cv::Mat *normals,
+	                                  cv::Mat *color, 
+	                                  cv::Mat *existing_labels) = 0;
+
 };
 
-
-class EdithSegmentation : public IncrementalSegmentation{
+class EdithSegmentation : public IncrementalSegmentation {
 public:
 
+    void initInThread() { }
 
-    void initInThread(){
+    cv::Mat generateNewLabels(cv::Mat *depth, cv::Mat *normals, cv::Mat *color, 
+                              cv::Mat *existing_labels);
 
-    }
-
-    cv::Mat generateNewLabels(cv::Mat *depth, cv::Mat *normals, cv::Mat *color, cv::Mat *existingLabels);
 };
 
-
-
-
-
-
-
-
-#endif //SUPERMAPPING_INCREMENTALSEGMENTATION_H
+#endif

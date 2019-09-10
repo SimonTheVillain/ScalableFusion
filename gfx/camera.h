bfcb88256aea58e8b8a5c45a65fe73bd77254e13
Nikolaus Wagner <e11722950@student.tuwien.ac.at>
2019-09-10 10:59:44 +0200

    style: Code cleanup of camera functions

--- gfx/camera.h
+++ gfx/camera.h
@@ -1,31 +1,23 @@
 #ifndef FILE_CAMERA_H
 #define FILE_CAMERA_H
+
 #include <eigen3/Eigen/Eigen>
 #include <opencv2/core.hpp>
 
-//using namespace Eigen;
-
-class Camera{
+class Camera {
 public:
-    static Eigen::Matrix4f lookFromAt(Eigen::Vector4f eye, Eigen::Vector4f center, Eigen::Vector4f up);
 
-    static Eigen::Matrix4f projection(float fovY, float aspectRatio, float zFar=100.0f, float zNear=0.01f);
+	static Eigen::Matrix4f lookFromAt(Eigen::Vector4f eye, Eigen::Vector4f center, 
+	                                  Eigen::Vector4f up);
+	static Eigen::Matrix4f projection(float fov_y, float aspect_ratio, 
+	                                  float z_far=100.0f, float z_near=0.01f);
 
-    /**
-     * @brief calcCamPosFromExtrinsic
-     * Todo: this doesn't really fit in here... (but maybe it does) i don't know actually
-     * @param cam
-     * @return
-     */
-    static Eigen::Vector4f calcCamPosFromExtrinsic(Eigen::Matrix4f cam);
+	static Eigen::Vector4f calcCamPosFromExtrinsic(Eigen::Matrix4f cam);
 
-    static Eigen::Matrix4f genProjMatrix(Eigen::Vector4f intrinsics);
-
-
-    static Eigen::Matrix4f genScaledProjMatrix(Eigen::Vector4f intrinsics,
-                                               cv::Size2i res);
+	static Eigen::Matrix4f genProjMatrix(Eigen::Vector4f intrinsics);
+	static Eigen::Matrix4f genScaledProjMatrix(Eigen::Vector4f intrinsics,
+	                                           cv::Size2i res);
 
 };
-
 
 #endif

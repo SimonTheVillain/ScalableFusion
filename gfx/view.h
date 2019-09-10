2b5b35d9bb64c9fb0d149a87e983bb4ad62d37a2
Nikolaus Wagner <e11722950@student.tuwien.ac.at>
2019-09-10 16:20:57 +0200

    style: Code cleanup off viewer

--- gfx/view.h
+++ gfx/view.h
@@ -1,16 +1,13 @@
 #ifndef FILE_VIEW_H
 #define FILE_VIEW_H
 
-
-
 /**
  * This is an ambitious interface to handle keyboard input within the opengl context
  * e.g. when multiple layers of rendering should be able to react to mouse and keyboard inputs
  */
-namespace gfx{
-class ViewController{
-private:
+namespace gfx {
 
+class ViewController {
 public:
     virtual bool isCursorResponder() {return true;}
     virtual bool isOnlyCursorResponder() {return true;}//says if the cursor should get captured
@@ -18,5 +15,7 @@ public:
     virtual bool isOnlyKeyboardResponder() {return true;}
     //now stuff to capture all this keyboard events
 };
-}
+
+} // namespace gfx
+
 #endif

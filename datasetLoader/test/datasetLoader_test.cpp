9aa0cc7decd49283194d433df84050b73de85052
Nikolaus Wagner <e11722950@student.tuwien.ac.at>
2019-09-10 09:55:28 +0200

    feat: Add test for datasetLoader

---
+++ datasetLoader/test/datasetLoader_test.cpp
@@ -0,0 +1,18 @@
+ #include "../datasetLoader.h"
+
+using namespace std;
+
+int main(int argc, const char* argv[]) {
+
+	string folder("./rgbd_dataset_freiburg3_teddy");
+	bool realtime = true;
+	bool use_pose = true;
+	bool use_high_res = false;
+	int skip_n_frames = 0;
+	float depth_scale = 1;
+	float trajectory_GT_scale = 1;
+	bool invert_GT_trajectory = false;
+	TumDataset dataset(folder); 
+
+	return 0;
+}

41d40198d7ddf5c7ca94b440409680eef0caa3d4
Nikolaus Wagner <nikolaus.wagner@outlook.com>
2019-09-12 11:18:37 +0000

    style: Code cleanup of intermediate depthmodel

--- mapping/intermediateDepthModel.cpp
+++ mapping/intermediateDepthModel.cpp
@@ -4,51 +4,42 @@
 #include <cuda.h>
 #include "cuda/reproject.h"
 
-using namespace Eigen;
 using namespace std;
-using namespace cv;
+using namespace Eigen;
 
+IntermediateMap::IntermediateMap(int sensor_width, int sensor_height, 
+                                 Vector4f depth_intrinsics) 
+		: width_(sensor_width),
+		  height_(sensor_height),
+		  d_intrinsics_(depth_intrinsics) {
 
-
-IntermediateMap::IntermediateMap(int sensorWidth, int sensorHeight, Eigen::Vector4f depthIntrinsics)
-{
-
-    this->width=sensorWidth;
-    this->height=sensorHeight;
-    cudaMalloc(&inDepth,sizeof(uint16_t)*width*height);
-    cudaMalloc(&outDepth16,sizeof(uint16_t)*width*height);
-    cudaMalloc(&outDepth32,sizeof(uint32_t)*width*height);
-    this->dIntrinsics = depthIntrinsics;
+	cudaMalloc(&in_depth_,     sizeof(uint16_t) * width_ * height_);
+	cudaMalloc(&out_depth_16_, sizeof(uint16_t) * width_ * height_);
+	cudaMalloc(&out_depth_32_, sizeof(uint32_t) * width_ * height_);
 
 }
 
-IntermediateMap::~IntermediateMap()
-{
-    cudaFree(inDepth);
-    cudaFree(outDepth16);
-    cudaFree(outDepth32);
+IntermediateMap::~IntermediateMap() {
+	cudaFree(in_depth_);
+	cudaFree(out_depth_16_);
+	cudaFree(out_depth_32_);
 }
 
-
-
-void IntermediateMap::setDepthMap(cv::Mat depth16, Eigen::Matrix4f poseAtCapture)
-{
-    this->camPoseAtCapture = poseAtCapture;
-
-    cudaMemcpy(inDepth,depth16.data,sizeof(uint16_t)*width*height,cudaMemcpyHostToDevice);
+void IntermediateMap::setDepthMap(cv::Mat depth_16, Matrix4f pose_at_capture) {
+	cam_pose_at_capture_ = pose_at_capture;
+	cudaMemcpy(in_depth_, depth_16.data, sizeof(uint16_t) * width_ * height_, 
+	           cudaMemcpyHostToDevice);
 }
 
-cv::Mat IntermediateMap::renderDepthMap(Eigen::Matrix4f pose)
-{
-    Eigen::Matrix4f poseTransform = pose.inverse()*camPoseAtCapture;
+cv::Mat IntermediateMap::renderDepthMap(Matrix4f pose) {
+	Matrix4f pose_transform = pose.inverse() * cam_pose_at_capture_;
 
-    cudaMemset(outDepth32,255,sizeof(uint32_t)*width*height);
-    reproject(inDepth,
-              outDepth16,outDepth32,
-              width,height,
-              poseTransform,
-              dIntrinsics);
-    Mat result(height,width,CV_16UC1);
-    cudaMemcpy(result.data,outDepth16,sizeof(uint16_t)*width*height,cudaMemcpyDeviceToHost);
-    return result;
+	cudaMemset(out_depth_32_, 255, sizeof(uint32_t) * width_ * height_);
+	reproject(in_depth_, out_depth_16_, out_depth_32_,  width_, height_,
+	          pose_transform, d_intrinsics_);
+	cv::Mat result(height_, width_, CV_16UC1);
+	cudaMemcpy(result.data, out_depth_16_, sizeof(uint16_t) * width_ * height_,
+	           cudaMemcpyDeviceToHost);
+
+	return result;
 }
--- mapping/intermediateDepthModel.h
+++ mapping/intermediateDepthModel.h
@@ -1,60 +1,53 @@
 #ifndef FILE_INTERMEDIATE_DEPTH_MODEL_H
 #define FILE_INTERMEDIATE_DEPTH_MODEL_H
 
-//#include <iostream>
 #include <opencv2/core.hpp>
 #include <Eigen/Eigen>
 #include <GL/glew.h>
 #include <shader.h>
-
-
 
 /*
  * TODO: create an intermediate model of stuff that isn't put into the reconstruction
  * already. this should be used for rendering. and mainly camera tracking
  */
 
+class IntermediateMap {
+public:
 
+	IntermediateMap(int width,int height, Eigen::Vector4f d_intrinsics);
+	~IntermediateMap();
 
-class IntermediateMap{
+	//maybe also add color
+	void setDepthMap(cv::Mat depth16,Eigen::Matrix4f pose_at_capture);
+
+	cv::Mat renderDepthMap(Eigen::Matrix4f pose);
+
 private:
 
-    /* ideally this should be done in opengl... but we are lazy and therefore are doing it in
-     * cuda
-    gfx::GLSLProgram program;
+	/* ideally this should be done in opengl... but we are lazy and therefore are doing it in
+	 * cuda
+	gfx::GLSLProgram program;
 
-    GLuint vertexBuffer;
-    GLuint indexBuffer;
-    GLuint VAO;
-
-
-    GLuint FBO;
-    GLuint depthRenderTex;
-    GLuint depthBuf;
-    */
-    int width;
-    int height;
-
-    //both buffers reside on gpu
-    uint16_t *inDepth;
-    uint32_t *outDepth32;
-    uint16_t *outDepth16;
-
-    Eigen::Vector4f dIntrinsics;
-
-    Eigen::Matrix4f camPoseAtCapture;
-
-public:
-    IntermediateMap(int width,int height,Eigen::Vector4f dIntrinsics);
-    ~IntermediateMap();
+	GLuint vertexBuffer;
+	GLuint indexBuffer;
+	GLuint VAO;
 
 
-    //void initializeInContext(int width,int height);
+	GLuint FBO;
+	GLuint depthRenderTex;
+	GLuint depthBuf;
+	*/
+	int width_;
+	int height_;
 
-    //maybe also add color
-    void setDepthMap(cv::Mat depth16,Eigen::Matrix4f poseAtCapture);
+	// both buffers reside on gpu
+	uint16_t *in_depth_;
+	uint32_t *out_depth_32_;
+	uint16_t *out_depth_16_;
 
-    cv::Mat renderDepthMap(Eigen::Matrix4f pose);
+	Eigen::Vector4f d_intrinsics_;
+
+	Eigen::Matrix4f cam_pose_at_capture_;
 
 };
 

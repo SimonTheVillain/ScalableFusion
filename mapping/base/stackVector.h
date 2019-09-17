661175b73a6ebb4423661a8f9f5c06e4d183491f
Nikolaus Wagner <nikolaus.wagner@outlook.com>
2019-09-17 10:16:24 +0000

    style: Code cleanup of stack vector

--- mapping/base/stackVector.h
+++ mapping/base/stackVector.h
@@ -1,9 +1,5 @@
-//
-// Created by simon on 5/5/19.
-//
-
-#ifndef SUPERMAPPING_STACKVECTOR_H
-#define SUPERMAPPING_STACKVECTOR_H
+#ifndef FILE_STACK_VECTOR_H
+#define FILE_STACK_VECTOR_H
 
 #include <vector>
 #include <assert.h>
@@ -13,149 +9,132 @@
 //stackoverflow:
 //https://stackoverflow.com/questions/354442/looking-for-c-stl-like-vector-class-but-using-stack-storage
 
+using namespace std;
 
-
-template<typename T,std::size_t static_capacity>
-class stack_vector{
+template<typename T, size_t StaticCapacity>
+class StackVector {
 public:
-    T stack[static_capacity];
-    std::size_t count = 0;
-    T* heap = nullptr;
-    std::size_t heap_capacity=0;
 
-    void push_back(T in){
-        if(count < static_capacity){
-            //nothing if we are within stack
-            stack[count] = in;
-        }else{
-            std::size_t delta = count-static_capacity;
-            //if we are running out of heap we need to reallocate some more
+	StackVector() {
+		//the constructor doesn't do much... since we don't really allocate anything
+	}
 
-            if(delta+1 > heap_capacity){
-                T* oldHeap = heap;
-                heap_capacity = heap_capacity*2;
-                if(heap_capacity==0){
-                    heap_capacity=2;
-                }
-                heap = new T[heap_capacity];
-                for(std::size_t i=0;i<delta;i++){
-                    heap[i] = oldHeap[i];
-                }
-                delete[] oldHeap;
-            }
-            heap[delta] = in;
-        }
-        count++;
-    }
-    void pop_back(){
-        count--;
+	StackVector(const StackVector<T, StaticCapacity> &in) 
+			: heap_capacity(in.heap_capacity),
+			  count(in.count) { //<T,static_capacity> (why is this not called?
+		memcpy(stack, in.stack, sizeof(T) * min(StaticCapacity, count));
+		if(count > StaticCapacity) {
+			heap = new T[heap_capacity];
+			size_t delta = count - StaticCapacity;
+			memcpy(heap, in.heap, sizeof(T) * delta);
+		}
+	}
 
-    }
-    void emplace_back(){
-        if(count < static_capacity){
-            //nothing if we are within stack
-        }else{
-            std::size_t delta = count-static_capacity;
-            //if we are running out of heap we need to reallocate some more
-            if(delta+1 > heap_capacity){
-                T* oldHeap = heap;
-                heap_capacity = heap_capacity*2;
-                if(heap_capacity==0){
-                    heap_capacity=2;
-                }
-                heap = new T[heap_capacity];
-                for(std::size_t i=0;i<delta;i++){
-                    heap[i] = oldHeap[i];
-                }
-                delete[] oldHeap;
-            }
-        }
-        count++;
-    }
-    std::size_t size(){
-        return count;
-    }
+	~StackVector() {
+		if(heap != nullptr) {
+			delete[] heap;
+		}
+	}
 
-    T& operator[](std::size_t i){
-        if(i<static_capacity){
-            return stack[i];
-        }
-        return heap[i-static_capacity];
-    }
+	void pushBack(T in) {
+		if(count < StaticCapacity) {
+			// Nothing if we are within stack
+			stack[count] = in;
+		} else {
+			size_t delta = count - StaticCapacity;
+			// If we are running out of heap we need to reallocate some more
+			if(delta + 1 > heap_capacity) {
+				T* old_heap = heap;
+				heap_capacity = heap_capacity * 2;
+				if(heap_capacity == 0) {
+					heap_capacity = 2;
+				}
+				heap = new T[heap_capacity];
+				for(size_t i = 0; i < delta; i++) {
+					heap[i] = old_heap[i];
+				}
+				delete[] old_heap;
+			}
+			heap[delta] = in;
+		}
+		count++;
+	}
 
-    stack_vector(){
-        //the constructor doesn't do much... since we don't really allocate anything
-    }
+	void popBack() {
+		count--;
+	}
 
-    stack_vector(const stack_vector<T,static_capacity>  &in){ //<T,static_capacity> (why is this not called?
-        heap_capacity = in.heap_capacity;
-        count = in.count;
-        memcpy(stack,in.stack,sizeof(T)*std::min(static_capacity, count));
-        if(count > static_capacity){
-            heap = new T[heap_capacity];
+	void emplaceBack() {
+		if(count < StaticCapacity) {
+			// Nothing if we are within stack
+		} else {
+			size_t delta = count - StaticCapacity;
+			// If we are running out of heap we need to reallocate some more
+			if(delta + 1 > heap_capacity) {
+				T* old_heap = heap;
+				heap_capacity = heap_capacity * 2;
+				if(heap_capacity == 0) {
+					heap_capacity = 2;
+				}
+				heap = new T[heap_capacity];
+				for(size_t i = 0; i< delta; i++) {
+					heap[i] = old_heap[i];
+				}
+				delete[] old_heap;
+			}
+		}
+		count++;
+	}
 
-            size_t delta = count - static_capacity;
-            memcpy(heap,in.heap,sizeof(T)*delta);
+	T& back() {
+		size_t i = count - 1;
+		if(i < StaticCapacity) {
+			return stack[i];
+		} else {
+			return heap[i-StaticCapacity];
+		}
+	}
 
-        }
-    }
+	size_t size() {
+		return count;
+	}
 
+	T& operator[](size_t i) {
+		if(i < StaticCapacity) {
+			return stack[i];
+		} else {
+			return heap[i - StaticCapacity];
+		}
+	}
 
-    ~stack_vector(){
-        if(heap!=nullptr){
-            delete[] heap;
-        }
-    }
+	//copy assignment
+	StackVector& operator=(const StackVector& that) {
+		heap_capacity = that.heap_capacity;
+		count = that.count;
+		memcpy(stack, that.stack, sizeof(T) * min(StaticCapacity, count));
+		if(count > StaticCapacity) {
+			heap = new T[heap_capacity];
+			size_t delta = count - StaticCapacity;
+			memcpy(heap, that.heap, sizeof(T) * delta);
+		}
+		return *this;
+	}
 
-    //copy assignment
-    stack_vector& operator=(const stack_vector& that){
-        heap_capacity = that.heap_capacity;
-        count = that.count;
-        memcpy(stack,that.stack,sizeof(T)*std::min(static_capacity, count));
-        if(count > static_capacity){
-            heap = new T[heap_capacity];
+	// move assignment
+	StackVector& operator=(StackVector&& that) {
+		heap_capacity = that.heap_capacity;
+		count = that.count;
+		memcpy(stack, that.stack, sizeof(T) * min(StaticCapacity, count));
+		heap = that.heap;
+		that.count = 0;
+		that.heap  = nullptr;
+		return *this;
+	}
 
-            size_t delta = count - static_capacity;
-            memcpy(heap,that.heap,sizeof(T)*delta);
-
-        }
-
-        //std::cout << "calling copy assignment" << std::endl;
-        return *this;
-    }
-
-
-
-    // move assignment
-    stack_vector& operator=(stack_vector&& that){
-
-        heap_capacity = that.heap_capacity;
-        count = that.count;
-        memcpy(stack,that.stack,sizeof(T)*std::min(static_capacity, count));
-        heap = that.heap;
-        that.count = 0;
-        that.heap = nullptr;
-        //std::cout << "calling move assignment" << std::endl;
-        return *this;
-    }
-
-
-    //copy/move constructor (i don't think this applies in this case)
-    /*
-    stack_vector& operator=(T that) noexcept {
-
-    }
-     */
-
-    T& back(){
-        size_t i = count-1;
-        if(i<static_capacity){
-            return stack[i];
-        }
-        return heap[i-static_capacity];
-    }
-
+	T  stack[StaticCapacity];
+	T* heap = nullptr;
+	size_t count = 0;
+	size_t heap_capacity = 0;
 };
 
-
-#endif //SUPERMAPPING_STACKVECTOR_H
+#endif

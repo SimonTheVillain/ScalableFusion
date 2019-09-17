1a9ad7ca08ae38b18cc36dd89b8cd176c619024a
Nikolaus Wagner <nikolaus.wagner@outlook.com>
2019-09-17 14:14:43 +0000

    style: Code cleanup of coarse update

--- mapping/cuda/coarseUpdate.cu
+++ mapping/cuda/coarseUpdate.cu
@@ -1,183 +1,164 @@
 #include "coarseUpdate.h"
-#include "gpuErrchk.h"
 
 //stupid debug shit!!!
 #include <iostream>
+
+#include "gpuErrchk.h"
+
 using namespace std;
-
 using namespace Eigen;
-__global__ void reduceColorAtPatches_kernel(CalcMeanColorDescriptor* descriptors,GpuCoarseVertex* vertices,Vector4f *colors){
-    extern __shared__ Vector4f s_colors[];//this should not necessarily hardcoded
-    const int k = blockIdx.x;
-    Vector4f color(0,0,0,0);
 
-    //sum up over all the elements
-    CalcMeanColorDescriptor &desc = descriptors[k];
+__global__ 
+void reduceColorAtPatches_kernel(CalcMeanColorDescriptor *descriptors,
+                                 GpuCoarseVertex *vertices, Vector4f *colors) {
+	extern __shared__ Vector4f s_colors[];//this should not necessarily hardcoded
+	const int k = blockIdx.x;
+	Vector4f color(0, 0, 0, 0);
 
+	//sum up over all the elements
+	CalcMeanColorDescriptor &desc = descriptors[k];
 
-    if(desc.vertInd < 0){
-        return;
-    }
+	if(desc.vert_ind < 0) {
+		return;
+	}
 
-    int absolutePixCount = desc.height*desc.width;
+	int absolute_pix_count = desc.height * desc.width;
 
-    int i=threadIdx.x;
-    while(i<absolutePixCount){
-        int x=i%desc.width + desc.x;
-        int y=i/desc.width + desc.y;
+	int i = threadIdx.x;
+	while(i < absolute_pix_count) {
+		int x = i % desc.width + desc.x;
+		int y = i / desc.width + desc.y;
 
-        Vector4f pixColor;
-        if(desc.hdr){
-            //if the image is stored as hdr we load it as such
-            printf("TODO!!!!! \n");
-        }else{
-            //otherwise we load it as 8 bit per color image
-            uchar4 c;
-            surf2Dread(&c,desc.color,x*sizeof(uchar4),y);
-            pixColor = Vector4f(c.x,c.y,c.z,c.w)*(desc.scale/255.0f);
-            if(k==0){
-                //printf("%d %d %d %d \n",int(c.x),int(c.y),int(c.z),int(c.w));
-                //printf("%f %f %f %f \n",pixColor[0],pixColor[1],pixColor[2],pixColor[3]);
-            }
-        }
-        color+=pixColor;
+		Vector4f pix_color;
+		if(desc.hdr) {
+			//if the image is stored as hdr we load it as such
+			printf("TODO!!!!! \n");
+		} else {
+			//otherwise we load it as 8 bit per color image
+			uchar4 c;
+			surf2Dread(&c, desc.color, x * sizeof(uchar4), y);
+			pix_color = Vector4f(c.x, c.y, c.z, c.w) * (desc.scale / 255.0f);
+		}
+		color += pix_color;
 
-        i+=blockDim.x;
-    }
+		i += blockDim.x;
+	}
 
+	//Do reduction to get the mean color
+	int tid = threadIdx.x;
+	s_colors[tid] = color;
+	__syncthreads();
 
-
-    //Do reduction to get the mean color
-    int tid = threadIdx.x;
-    s_colors[tid]=color;
-    __syncthreads();
-    for(int s=blockDim.x/2;s>0;s>>=1){
-        if(tid>=s){
-            //return;//TODO: maybe add this back in!?
-        }
-        if(tid<s){
-            s_colors[tid]+=s_colors[tid+s];
-        }
-        __syncthreads();
-    }
-    if(tid==0 &&  desc.vertInd >= 0){
-        vertices[desc.vertInd].c = s_colors[0]*(1.0f/float(absolutePixCount));
-    }
-    //we should be able to get rid of the loop
-    //it will be different beginning with cuda 9 though
-    //http://developer.download.nvidia.com/compute/cuda/1.1-Beta/x86_website/projects/reduction/doc/reduction.pdf
+	for(int s = blockDim.x / 2; s > 0; s >>= 1) {
+		if(tid >= s) {
+			//return;//TODO: maybe add this back in!?
+		}
+		if(tid < s) {
+			s_colors[tid] += s_colors[tid + s];
+		}
+		__syncthreads();
+	}
+	if(tid == 0 &&  desc.vert_ind >= 0) {
+		vertices[desc.vert_ind].c = s_colors[0] * (1.0f / float(absolute_pix_count));
+	}
+	//we should be able to get rid of the loop
+	//it will be different beginning with cuda 9 though
+	//http://developer.download.nvidia.com/compute/cuda/1.1-Beta/x86_website/projects/reduction/doc/reduction.pdf
 }
 
+void calcMeanColor(vector<CalcMeanColorDescriptor> descriptors,
+                   GpuCoarseVertex* vertices, Vector4f* colors) {
+	if(descriptors.empty()) {
+		return;
+	}
+		//lets do 1024 threads for each patch
+	dim3 block(1024);
+	dim3 grid(descriptors.size());
 
-void calcMeanColor(std::vector<CalcMeanColorDescriptor> descriptors,
-                                           GpuCoarseVertex* vertices,
-                                           Vector4f* colors){
-    if(descriptors.size()==0){
-        return;
-    }
-        //lets do 1024 threads for each patch
-    dim3 block(1024);
-    dim3 grid(descriptors.size());
+	Vector4f* colors_gpu = 0;
+	if(colors) {
+		cudaMalloc(&colors_gpu, descriptors.size() * sizeof(Vector4f));
+	}
 
-    Vector4f* colorsGpu = 0;
-    if(colors){
-        cudaMalloc(&colorsGpu,descriptors.size()*sizeof(Vector4f));
-    }
+	CalcMeanColorDescriptor *descs;
+	cudaMalloc(&descs, descriptors.size() * sizeof(CalcMeanColorDescriptor));
+	cudaMemcpy(descs, &descriptors[0], 
+	           descriptors.size() * sizeof(CalcMeanColorDescriptor), 
+	           cudaMemcpyHostToDevice);
 
-    CalcMeanColorDescriptor *descs;
-    cudaMalloc(&descs,descriptors.size()*sizeof(CalcMeanColorDescriptor));
-    cudaMemcpy(descs,&descriptors[0],descriptors.size()*sizeof(CalcMeanColorDescriptor),cudaMemcpyHostToDevice);//copy it to the device
+	gpuErrchk(cudaPeekAtLastError());
 
-    gpuErrchk( cudaPeekAtLastError() );
+	reduceColorAtPatches_kernel<<<grid, block, sizeof(Vector4f) * block.x>>>(
+			descs,vertices,colors_gpu);
 
+	cudaDeviceSynchronize();
+	gpuErrchk(cudaPeekAtLastError());
+	cudaFree(descs);
 
-    //do the kernel call
-    reduceColorAtPatches_kernel<<< grid,block,
-            sizeof(Vector4f)*block.x>>>(descs,vertices,colorsGpu);
-
-    cudaDeviceSynchronize();
-    gpuErrchk( cudaPeekAtLastError() );
-    cudaFree(descs);
-
-    if(colors){
-        cudaMemcpy(colorsGpu,colors,descriptors.size()*sizeof(Vector4f),cudaMemcpyDeviceToHost);//copy it to the device
-        cudaFree(colorsGpu);
-
-        cudaDeviceSynchronize();
-        gpuErrchk( cudaPeekAtLastError() );
-    }
+	if(colors) {
+		cudaMemcpy(colors_gpu, colors, descriptors.size() * sizeof(Vector4f), 
+		           cudaMemcpyDeviceToHost);
+		cudaFree(colors_gpu);
+		cudaDeviceSynchronize();
+		gpuErrchk(cudaPeekAtLastError());
+	}
 }
 
-
-__global__ void setVisibility_kernel(   int* visibIndices,
-                                        size_t nrIndices,
-                                        int* gpuVisBuffer,
-                                        int value){
-    const int i = threadIdx.x + blockIdx.x*blockDim.x;
-    if(i>=nrIndices){
-        return;
-    }
-
-    int index=visibIndices[i];
-    gpuVisBuffer[index]=value;
+__global__ 
+void setVisibility_kernel(int *visib_indices, size_t nr_indices,
+                          int *gpu_vis_buffer, int value) {
+	const int i = threadIdx.x + blockIdx.x * blockDim.x;
+	if(i >= nr_indices) {
+		return;
+	}
+	int index = visib_indices[i];
+	gpu_vis_buffer[index] = value;
 }
 
+void coarseUpdateVisibility(vector<int> enable_patches, 
+                            vector<int> disable_patches,
+                            int *gpu_vis_buffer) {
+	cudaDeviceSynchronize();
+	gpuErrchk(cudaPeekAtLastError());
+	int *data;
+	cudaMalloc(&data, 
+	           sizeof(int) * max(enable_patches.size(), disable_patches.size()));
 
-void coarseUpdateVisibility(std::vector<int> enablePatches,
-                            std::vector<int> disablePatches,
-                            int* gpuVisBuffer){
+	dim3 block(256);
+	dim3 grid(enable_patches.size() / block.x + 1);
 
-    cudaDeviceSynchronize();
-    gpuErrchk( cudaPeekAtLastError() );
-    int *data;
-    cudaMalloc(&data,sizeof(int)*std::max(enablePatches.size(),disablePatches.size()));
+	cudaMemcpy(data, &enable_patches[0], enable_patches.size() * sizeof(int),
+	           cudaMemcpyHostToDevice);
 
-    //cout << data << endl;
-    dim3 block(256);
+	cudaDeviceSynchronize();
+	gpuErrchk(cudaPeekAtLastError());
 
-    dim3 grid(enablePatches.size()/block.x+1);
+	if(!enable_patches.empty()) {
+		setVisibility_kernel<<<grid, block>>>(data, enable_patches.size(), 
+		                                      gpu_vis_buffer, 1);
+	}
 
-    cudaMemcpy(data,&enablePatches[0],
-            enablePatches.size()*sizeof(int),
-            cudaMemcpyHostToDevice);//copy it to the device
+	cudaDeviceSynchronize();
+	gpuErrchk(cudaPeekAtLastError());
 
-    cudaDeviceSynchronize();
-    gpuErrchk( cudaPeekAtLastError() );
+	grid.x = disable_patches.size() / block.x + 1;
 
-    if( enablePatches.size() != 0 ){
-        setVisibility_kernel<<<grid,block>>>(data,
-                                             enablePatches.size(),
-                                             gpuVisBuffer,1);//1
-    }
+	cudaMemcpy(data, &disable_patches[0], disable_patches.size() * sizeof(int),
+	          cudaMemcpyHostToDevice);
+	if(!disable_patches.empty()) {
+		setVisibility_kernel<<<grid, block>>>(data, disable_patches.size(),
+		                                      gpu_vis_buffer, 0);
+	}
 
-    cudaDeviceSynchronize();
-    gpuErrchk( cudaPeekAtLastError() );
+	cudaError err = cudaPeekAtLastError();
+	if(err != cudaSuccess) {
+		cout << data << endl;// i fear the pointer is empty for some weird reason
+	}
+	cudaDeviceSynchronize();
+	gpuErrchk(err);
 
+	cudaFree(data);
 
-
-    grid.x=disablePatches.size()/block.x+1;
-
-    cudaMemcpy(data,&disablePatches[0],
-            disablePatches.size()*sizeof(int),
-            cudaMemcpyHostToDevice);//copy it to the device
-    if( disablePatches.size() != 0 ){
-        setVisibility_kernel<<<grid,block>>>(data,
-                                             disablePatches.size(),
-                                             gpuVisBuffer,0);
-    }
-
-
-
-    cudaError err = cudaPeekAtLastError();
-    if(err != cudaSuccess){
-        cout << data << endl;// i fear the pointer is empty for some weird reason
-    }
-    cudaDeviceSynchronize();
-    gpuErrchk( err );
-
-    cudaFree(data);
-
-    cudaDeviceSynchronize();
-    gpuErrchk( cudaPeekAtLastError() );
-
+	cudaDeviceSynchronize();
+	gpuErrchk(cudaPeekAtLastError());
 }
--- mapping/cuda/coarseUpdate.h
+++ mapping/cuda/coarseUpdate.h
@@ -1,68 +1,56 @@
 #ifndef FILE_COARSE_UPDATE_H
 #define FILE_COARSE_UPDATE_H
 
-#include <Eigen/Eigen>
+#include <vector>
 
+#include <Eigen/Eigen>
 #include <cuda.h>
 #include <cublas.h>
 
-#include <vector>
-
-
 #include "gpuMeshStructure.h"
 
+using namespace std;
 
 //TODO: the next two functions could be generalized (its a index attached to some structure to store
-struct CoarseUpdateVisDescriptor{
-    int vertInd=-1;//the index of the vector
-    int visibility=1;
+struct CoarseUpdateVisDescriptor {
+	int vert_ind   = -1;//the index of the vector
+	int visibility = 1;
 };
 
-
-void coarseUpdateVis(std::vector<CoarseUpdateVisDescriptor> descriptors,int* gpuVisBuf);
-
-
-void coarseUpdateVisibility(std::vector<int> enablePatches, std::vector<int> disablePatches,int* gpuVisBuffer);
-
-struct CoarseUpdateVertDescriptor{
-    int vertInd=-1;//the index of the
-    GpuCoarseVertex vert;
-
+struct CoarseUpdateVertDescriptor {
+	int vert_ind = -1;//the index of the vector
+	GpuCoarseVertex vert;
 };
 
+struct CalcMeanColorDescriptor {
+	int vert_ind = -1;
+	float scale = 1.0f; // scale the color (i.e. when its normalized)
 
-void coarseUpdateVert(std::vector<CoarseUpdateVisDescriptor> descriptors,GpuCoarseVertex* vertices);
-
-
-
-struct CalcMeanColorDescriptor{
-    int vertInd=-1;
-    //set the
-    float scale=1.0f;//scale the color (i.e. when its normalized)
-
-    //reference to the image and where on the image
-    cudaSurfaceObject_t color;
-    bool hdr = false;
-    int x;
-    int y;
-    int width;
-    int height;
+	//reference to the image and where on the image
+	cudaSurfaceObject_t color;
+	bool hdr = false;
+	int x;
+	int y;
+	int width;
+	int height;
 };
 
-void calcMeanColor(std::vector<CalcMeanColorDescriptor> descriptors, GpuCoarseVertex* vertices, Eigen::Vector4f *colors);
-
-
-struct CalcPatchCenter{
-    //todo:
-    //reference to the patch and information of its size.
-
-
-    int storeAtVertex=-1;
+struct CalcPatchCenter {
+	//todo:
+	//reference to the patch and information of its size.
+	int store_at_vertex = -1;
 };
 
+void coarseUpdateVis(vector<CoarseUpdateVisDescriptor> descriptors,
+                     int *gpuVisBuf);
 
+void coarseUpdateVisibility(vector<int> enablePatches, 
+                            vector<int> disable_patches, int *gpu_vis_buffer);
 
+void coarseUpdateVert(vector<CoarseUpdateVisDescriptor> descriptors,
+                      GpuCoarseVertex *vertices);
 
-
+void calcMeanColor(vector<CalcMeanColorDescriptor> descriptors, 
+                   GpuCoarseVertex *vertices, Eigen::Vector4f *colors);
 
 #endif

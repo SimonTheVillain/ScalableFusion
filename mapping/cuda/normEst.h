ca4fd0f4c8b6a8b89cdf2bf990b7eb3b55fd97d7
Nikolaus Wagner <nikolaus.wagner@outlook.com>
2019-09-18 09:26:02 +0000

    style: Code cleanup of normal estimation

--- mapping/cuda/normEst.h
+++ mapping/cuda/normEst.h
@@ -1,22 +1,17 @@
 #ifndef FILE_NORM_EST_H
 #define FILE_NORM_EST_H
+
 #include <cublas.h>
 #include <Eigen/Eigen>
 
-
-void cudaCalcNormals(cudaSurfaceObject_t dStdMinStd, cudaSurfaceObject_t points,
+void cudaCalcNormals(cudaSurfaceObject_t d_std_min_std, 
+                     cudaSurfaceObject_t points,
                      cudaSurfaceObject_t normals,
-                     int width, int height,float threshold);
+                     int width, int height, float threshold);
 
-
-
-
-
-void cudaCalcPoints(cudaSurfaceObject_t dStdMinStd, cudaSurfaceObject_t points,
-                int width, int height, Eigen::Vector4f fxycxy);
-
-
-
+void cudaCalcPoints(cudaSurfaceObject_t d_std_min_std, 
+                    cudaSurfaceObject_t points,
+                    int width, int height, Eigen::Vector4f fxycxy);
 
 #endif
 

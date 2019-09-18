d95194fb64d0f931bc82006d008293a1b5cec2f4
Nikolaus Wagner <nikolaus.wagner@outlook.com>
2019-09-18 08:53:26 +0000

    style: Code cleanup of labelling

--- mapping/cuda/labelling.cu
+++ mapping/cuda/labelling.cu
@@ -1,305 +1,239 @@
 #include "labelling.h"
+
 #include "gpuErrchk.h"
 #include "surfaceRead.h"
 
+using namespace std;
 using namespace Eigen;
 
-namespace gpu{
+namespace gpu {
 
-__global__ void labelSurfaces_kernel(Labelling::SegProjTask* tasks,
-                                    const cudaSurfaceObject_t labelling,
-                                    const cudaSurfaceObject_t geomBuffer,//the depth and geometry at the current view
-                                    cv::Size2i resolution,
-                                    Eigen::Matrix4f _pose,
-                                    Eigen::Matrix4f proj_pose,
-                                    GpuVertex *vertices,
-                                    Eigen::Vector2f *texPos,
-                                    GpuTriangle* triangles,
-                                    GpuPatchInfo* patchInfos){
+__global__ 
+void labelSurfaces_kernel(Labelling::SegProjTask* tasks,
+                          const cudaSurfaceObject_t labelling,
+                          const cudaSurfaceObject_t geom_buffer,//the depth and geometry at the current view
+                          cv::Size2i resolution,
+                          Matrix4f _pose,
+                          Matrix4f proj_pose,
+                          GpuVertex *vertices,
+                          Vector2f *tex_pos,
+                          GpuTriangle *triangles,
+                          GpuPatchInfo *patch_infos) {
 
-    const int width = resolution.width;
-    const int height = resolution.height;
-    uint32_t k = blockIdx.x;
-    Labelling::SegProjTask &task=tasks[k];
-    uint32_t i= threadIdx.x;
+	const int width = resolution.width;
+	const int height = resolution.height;
+	uint32_t k = blockIdx.x;
+	Labelling::SegProjTask &task = tasks[k];
+	uint32_t i = threadIdx.x;
 
+	uint32_t vertex_source_offset = task.vertex_dest_start_ind;
+	//uint32_t vertexDestOffset = task.vertexDestinationStartInd;
 
-    uint32_t vertexSourceOffset = task.vertexDestStartInd;
-    //uint32_t vertexDestOffset = task.vertexDestinationStartInd;
+	int pixel_count = task.destination.width * task.destination.height;
 
-    int pixelCount = task.destination.width*task.destination.height;
-    //int l=0;
-    while(i<pixelCount){
-        //same procedure as in stdTexUpdate.cu
-        int x=i%task.destination.width;
-        int y=i/task.destination.width;
+	while(i < pixel_count) {
+		// same procedure as in stdTexUpdate.cu
+		int x = i % task.destination.width;
+		int y = i / task.destination.width;
 
-        int xRef = x+task.lookup.x;
-        int yRef = y+task.lookup.y;
+		int x_ref = x + task.lookup.x;
+		int y_ref = y + task.lookup.y;
 
-        int xDest = x+task.destination.x;
-        int yDest = y+task.destination.y;
+		int x_dest = x + task.destination.x;
+		int y_dest = y + task.destination.y;
 
+		// DEBUG THINGY END THIS AND PUT A UNIFORM COLOR IN PLACE!!!!!!!<
+		if(false) {
+			float4 data = make_float4(0.5f, 0, 0, 0);
+			surf2Dwrite(data, task.dest_surf, x_dest * sizeof(float4), y_dest);
+			i += blockDim.x;
+			continue;
+		}
 
-        //placeholder code for serious labelling!
-        //l++;
+		//readout the lookup shizzle
+		float4 ref;
+		if(x_ref>=1024 || y_ref>=1024) {
+			printf("This is not right!!!! %d, %d \n", x_ref, y_ref);
+		}
+		//TODO: find out why this is creating the memory exception
+		surf2Dread(&ref, task.lookup_surf, x_ref * sizeof(Vector4f), y_ref);
 
-        /*
-        surf2Dwrite(l*1000,task.destSurf,//l*1000
-                    xDest*sizeof(int4) + task.subchannel * sizeof(int),
-                    //this could lead to alignment issues
-                    yDest);
-        */
+		//get point in space shizzle:
+		float bary[3] = {ref.y, ref.z, ref.w};
+		int32_t triangle_id= *((int*) (&ref.x));
 
-        /*
-        int4 data = make_int4(100000,0,100000,0);
-        surf2Dwrite(data,task.destSurf,
-                    xDest*sizeof(int4),yDest);
+		if(triangle_id < 0) {
+			i += blockDim.x;
+			continue;
+		}
 
-        */
+		if(false) {
+			//another debug!!!!! The illegal memory access is already happening here!!!
+			float4 data = make_float4(0.5f, 0, 0, 0);
+			surf2Dwrite(data, task.dest_surf, x_dest * sizeof(float4), y_dest);
+			i += blockDim.x;
+			continue;
+		}
 
-        //DEBUG THINGY END THIS AND PUT A UNIFORM COLOR IN PLACE!!!!!!!<
-        if(false){
-            float4 data = make_float4(0.5f,0,0,0);
-            surf2Dwrite(data,task.destSurf,
-                        xDest*sizeof(float4),yDest);
-            i+=blockDim.x;
-            continue;
-        }
+		GpuTriangle &triangle = triangles[triangle_id];
 
-        //readout the lookup shizzle
-        float4 ref;
-        if(xRef>=1024 || yRef>=1024){
-            printf("This is not right!!!! %d, %d \n",xRef,yRef);
-        }
-        //TODO: find out why this is creating the memory exception
-        surf2Dread(&ref,task.lookupSurf,
-                   xRef*sizeof(Vector4f),yRef);
+		Vector4f point(0, 0, 0, 0);
+		for(int j = 0; j < 3; j++) {
+			point += vertices[vertex_source_offset + triangle.indices[j]].p * bary[j];
+		}
 
-        //get point in space shizzle:
-        float bary[3]= {ref.y,ref.z,ref.w};
-        int32_t triangleId= *((int*)(&ref.x));
+		if(k == 0) {
+			//debug
+			Vector4f p = _pose * point;
+		}
 
-        if(triangleId<0){
-            i+=blockDim.x;
-            continue;
-        }
+		//project point shizzle:
+		Vector4f tex_coord = proj_pose * point;
+		float u = tex_coord[0] / tex_coord[3];
+		float v = tex_coord[1] / tex_coord[3];
 
-        if(false){
-            //another debug!!!!! The illegal memory access is already happening here!!!
-            float4 data = make_float4(0.5f,0,0,0);
-            surf2Dwrite(data,task.destSurf,
-                        xDest*sizeof(float4),yDest);
-            i+=blockDim.x;
-            continue;
-        }
-
-
-        GpuTriangle &triangle = triangles[triangleId];
-
-        Vector4f point(0,0,0,0);
-        for(int j=0;j<3;j++){
-            point += vertices[vertexSourceOffset+triangle.indices[j]].p*bary[j];
-        }
+		int ui = round(u);
+		int vi = round(v);
 
 
-        if(k == 0){
-            //debug
-            Vector4f p = _pose * point;
+		if(u < 0 || u > width || v < 0 || v > height || true) {
+			//debug
+		}
 
-            //printf("%f %f %f %f \n",p[0],p[1],p[2],p[3]);
-        }
-        //project point shizzle:
-        Vector4f texCoord = proj_pose*point;
-        float u=texCoord[0]/texCoord[3];
-        float v=texCoord[1]/texCoord[3];
+		//TODO: read old label if we already have a label!!!!!!!!
+		int old_label;
+		int4 old_label4;
+		surf2Dread(&old_label4, task.dest_surf, x_dest * sizeof(int4), y_dest);
+		old_label = old_label4.x;
+		if(old_label != -1) {
+			//TODO: when creating the label textures for the patches they need to be initialized with -1
+			//don't overwrite the old label if it already exists
+			i += blockDim.x;
+			continue;
+		}
 
-        int ui = round(u);
-        int vi = round(v);
+		int new_label;
+		if(ui < 0 || vi < 0 || ui >= width || vi >= height ||
+		   u < 0 || v < 0 || u >= width || v >= height) {
+			//this should actually barely happen!!!!!!
+			//i+=blockDim.x;
+			//continue;
+			new_label = -1;
+			new_label = -4;//debug
+		} else {
+			//we are not out of bounds
+			float depth;
+			surf2Dread(&depth, geom_buffer, ui * sizeof(float), vi);
+			if(isnan(depth)) {
+				new_label = -1;
+				new_label = -2; // debug
+			} else {
+				float depth_threshold = 0.005f;//5mm should be enough (or we make it relative to the depth)
+				Vector4f pos_cam_frame = _pose * point;
+				if(fabs(pos_cam_frame[2] - depth) < depth_threshold) {
+					new_label = -1;
+					new_label = -3;//debug
+				} else {
+					//read the new label!!!!!
+					surf2Dread(&new_label, labelling, ui * sizeof(int), vi);
+					new_label = 100000;//debug
+				}
+			}
+		}
 
+		//write the label
+		surf2Dwrite(new_label, task.dest_surf, 
+		            x_dest * sizeof(Vector4i) + task.subchannel * sizeof(int),
+		            //this could lead to alignment issues
+		            y_dest);
 
-        if(u<0 || u>width || v<0 || v>height || true){
-            //debug
-
-            //printf("%f %f  \n",u,v);
-        }
-
-        //TODO: read old label if we already have a label!!!!!!!!
-        int oldLabel;
-        int4 oldLabel4;
-        surf2Dread(&oldLabel4,task.destSurf,
-                    xDest*sizeof(int4),yDest);
-        oldLabel = oldLabel4.x;
-        if(oldLabel != -1){
-            //TODO: when creating the label textures for the patches they need to be initialized with -1
-            //don't overwrite the old label if it already exists
-            i+=blockDim.x;
-            continue;
-        }
-
-
-
-        int newLabel;
-        if(ui<0 || vi<0 || ui>=width || vi>=height ||
-            u<0 || u<0 || u>=width || v>=height){
-            //this should actually barely happen!!!!!!
-            //i+=blockDim.x;
-            //continue;
-            newLabel=-1;
-            newLabel = -4;//debug
-        }else{
-            //we are not out of bounds
-            float depth;
-            surf2Dread(&depth,geomBuffer,ui*sizeof(float),vi);
-            if(isnan(depth)){
-                newLabel = -1;
-                newLabel = -2; // debug
-            }else{
-                float depthThreshold = 0.005f;//5mm should be enough (or we make it relative to the depth)
-                Vector4f posCamFrame = _pose*point;
-                if(fabs(posCamFrame[2] - depth) < depthThreshold){
-                    newLabel = -1;
-                    newLabel = -3;//debug
-                }else{
-                    //read the new label!!!!!
-                    surf2Dread(&newLabel,labelling,ui*sizeof(int),vi);
-                    newLabel = 100000;//debug
-                }
-
-            }
-
-
-            //now check if the
-
-        }
-
-
-        //write the label
-
-
-        surf2Dwrite(newLabel,task.destSurf,
-                    xDest*sizeof(Eigen::Vector4i) + task.subchannel * sizeof(int),
-                    //this could lead to alignment issues
-                    yDest);
-
-        i+=blockDim.x;
-    }
-
-
+		i+=blockDim.x;
+	}
 }
 
-void Labelling::labelSurfaces(std::vector<Labelling::SegProjTask> tasks,
+void Labelling::labelSurfaces(vector<Labelling::SegProjTask> tasks,
                               const cudaSurfaceObject_t labelling,
-                              const cudaSurfaceObject_t geomBuffer,//the depth and geometry at the current view
+                              const cudaSurfaceObject_t geom_buffer,//the depth and geometry at the current view
                               cv::Size2i resolution,
-                              Eigen::Matrix4f _pose,
-                              Eigen::Matrix4f proj_pose,
+                              Matrix4f _pose,
+                              Matrix4f proj_pose,
                               GpuVertex *vertices,
-                              Eigen::Vector2f *texPos,
+                              Vector2f *tex_pos,
                               GpuTriangle* triangles,
-                              GpuPatchInfo* patchInfos){
+                              GpuPatchInfo* patch_infos) {
 
+	if(tasks.empty()) {
+		return;
+	}
+	dim3 block(256);
+	dim3 grid(tasks.size());
+	size_t task_bytes = sizeof(Labelling::SegProjTask) * tasks.size();
+	Labelling::SegProjTask *tasks_gpu;
+	cudaMalloc(&tasks_gpu, task_bytes);
+	cudaMemcpy(tasks_gpu, &(tasks[0]), task_bytes, cudaMemcpyHostToDevice);
 
-    if(tasks.size()==0){
-        return;
-    }
-    dim3 block(256);
-    dim3 grid(tasks.size());
-    size_t taskBytes = sizeof(Labelling::SegProjTask)*tasks.size();
-    Labelling::SegProjTask *tasksGpu;
-    cudaMalloc(&tasksGpu,taskBytes);
-    cudaMemcpy(tasksGpu,&(tasks[0]),
-            taskBytes,
-            cudaMemcpyHostToDevice);
+	//TODO: call the kernel
+	labelSurfaces_kernel<<<grid, block>>>(tasks_gpu, labelling, geom_buffer,
+	                                      resolution, _pose, proj_pose, vertices,
+	                                      tex_pos, triangles, patch_infos);
 
-    //TODO: call the kernel
-    labelSurfaces_kernel<<<grid,block>>>(tasksGpu,
-                                         labelling,
-                                         geomBuffer,
-                                         resolution,
-                                         _pose,
-                                         proj_pose,
-                                         vertices,
-                                         texPos,
-                                         triangles,
-                                         patchInfos);
-
-    cudaFree(tasksGpu);
-    cudaDeviceSynchronize();
-    gpuErrchk( cudaPeekAtLastError() );
-
-
-
+	cudaFree(tasks_gpu);
+	cudaDeviceSynchronize();
+	gpuErrchk(cudaPeekAtLastError());
 }
 
+template<class T> __global__ 
+void initializeSurfaceLabels_kernel(Labelling::InitializeTask *tasks, T value) {
+	uint32_t k = blockIdx.x;
+	Labelling::InitializeTask &task = tasks[k];
+	uint32_t i = threadIdx.x;
 
-template<class T>
-__global__ void initializeSurfaceLabels_kernel(Labelling::InitializeTask* tasks,T value){
-    uint32_t k = blockIdx.x;
-    Labelling::InitializeTask &task=tasks[k];
-    uint32_t i= threadIdx.x;
+	int pixel_count = task.dest_rect.width * task.dest_rect.height;
+	while(i < pixel_count) {
 
+		int x = i % task.dest_rect.width;
+		int y = i / task.dest_rect.width;
 
+		int x_dest = x + task.dest_rect.x;
+		int y_dest = y + task.dest_rect.y;
 
-    int pixelCount = task.destRect.width*task.destRect.height;
-    while(i<pixelCount) {
+		surf2Dwrite(value, task.dest_surf, x_dest * sizeof(T), y_dest);
 
-        int x=i%task.destRect.width;
-        int y=i/task.destRect.width;
-
-        int xDest = x+task.destRect.x;
-        int yDest = y+task.destRect.y;
-
-        surf2Dwrite(value,task.destSurf,
-                    xDest*sizeof(T),
-                    yDest);
-
-
-        i+=blockDim.x;
-    }
+		i += blockDim.x;
+	}
 }
 
 template<class T>
-void Labelling::initializeSurfaces(std::vector<Labelling::InitializeTask> tasks, T value) {
-    if(tasks.size()==0){
-        return;
-    }
-    dim3 block(256);
-    dim3 grid(tasks.size());
+void Labelling::initializeSurfaces(vector<Labelling::InitializeTask> tasks, 
+                                   T value) {
+	if(tasks.empty()) {
+		return;
+	}
+	dim3 block(256);
+	dim3 grid(tasks.size());
 
+	size_t task_bytes = sizeof(Labelling::InitializeTask) * tasks.size();
+	Labelling::InitializeTask *tasks_gpu;
+	cudaMalloc(&tasks_gpu, task_bytes);
+	cudaMemcpy(tasks_gpu, &(tasks[0]), task_bytes, cudaMemcpyHostToDevice);
 
-    size_t taskBytes = sizeof(Labelling::InitializeTask)*tasks.size();
-    Labelling::InitializeTask *tasksGpu;
-    cudaMalloc(&tasksGpu,taskBytes);
-    cudaMemcpy(tasksGpu,&(tasks[0]),
-               taskBytes,
-               cudaMemcpyHostToDevice);
+	initializeSurfaceLabels_kernel<<<grid, block>>>(tasks_gpu, value);
 
-
-    //do the kernel call!!!!!!
-    initializeSurfaceLabels_kernel<<<grid,block>>>(tasksGpu,value);
-
-
-    cudaFree(tasksGpu);
-    cudaDeviceSynchronize();
-    gpuErrchk( cudaPeekAtLastError() );
-
-
-
-
+	cudaFree(tasks_gpu);
+	cudaDeviceSynchronize();
+	gpuErrchk(cudaPeekAtLastError());
 }
 
 //specialization
 template
-void Labelling::initializeSurfaces(std::vector<Labelling::InitializeTask> tasks,
-                               int32_t value);
+void Labelling::initializeSurfaces(vector<Labelling::InitializeTask> tasks,
+                                   int32_t value);
 
 template<>
-void Labelling::initializeSurfaces(std::vector<Labelling::InitializeTask> tasks,
-                               Eigen::Vector4f value){
-    float4 value2 = make_float4(value[0],value[1],value[2],value[3]);
-    Labelling::initializeSurfaces(tasks,value2);
+void Labelling::initializeSurfaces(vector<Labelling::InitializeTask> tasks,
+                                   Vector4f value) {
+	float4 value2 = make_float4(value[0], value[1], value[2], value[3]);
+	Labelling::initializeSurfaces(tasks, value2);
 }
 
-
-}
+} // namespace gpu
--- mapping/cuda/labelling.h
+++ mapping/cuda/labelling.h
@@ -4,57 +4,55 @@
 //#define FILE_LABELLING_H
 
 //whatever will be going on in here
+#include <vector>
+
 #include <cuda.h>
 #include <cublas.h>
-#include <vector>
 #include <opencv2/core.hpp>
 #include <Eigen/Eigen>
 
 #include "gpuMeshStructure.h"
 #include "gpuErrchk.h"
 
-namespace gpu{
-    class Labelling{
-    public:
-        struct SegProjTask{
-            int subchannel;
-            cudaSurfaceObject_t lookupSurf;
-            cv::Point2i lookup;
-            cudaSurfaceObject_t destSurf;
-            cv::Rect2i destination;//we might need more than this
-            size_t vertexDestStartInd;
-        };
+using namespace std;
+using namespace Eigen;
 
-        static void labelSurfaces(std::vector<SegProjTask> tasks,
-                                  const cudaSurfaceObject_t labelling,
-                //the depth and geometry at the current view
-                                  const cudaSurfaceObject_t geomBuffer,
-                                  cv::Size2i resolution,
-                                  Eigen::Matrix4f _pose,
-                                  Eigen::Matrix4f proj_pose,
-                                  GpuVertex *vertices,
-                                  Eigen::Vector2f *texPos,
-                                  GpuTriangle* triangles,
-                                  GpuPatchInfo* patchInfos);
+namespace gpu {
 
-        struct InitializeTask{
-            cudaSurfaceObject_t destSurf;
-            cv::Rect2i destRect;
-        };
+	class Labelling {
+	public:
 
+		struct SegProjTask {
+			int subchannel;
+			cudaSurfaceObject_t lookup_surf;
+			cv::Point2i lookup;
+			cudaSurfaceObject_t dest_surf;
+			cv::Rect2i destination;//we might need more than this
+			size_t vertex_dest_start_ind;
+		};
 
-        template<class T>
-        static void initializeSurfaces(std::vector<InitializeTask> tasks,
-                                       T value);
+		struct InitializeTask {
+			cudaSurfaceObject_t dest_surf;
+			cv::Rect2i dest_rect;
+		};
 
+		static void labelSurfaces(vector<SegProjTask> tasks,
+		                          const cudaSurfaceObject_t labelling,
+		                          //the depth and geometry at the current view
+		                          const cudaSurfaceObject_t geom_buffer,
+		                          cv::Size2i resolution,
+		                          Matrix4f _pose,
+		                          Matrix4f proj_pose,
+		                          GpuVertex *vertices,
+		                          Vector2f *tex_pos,
+		                          GpuTriangle* triangles,
+		                          GpuPatchInfo* patch_infos);
 
+		template<class T>
+		static void initializeSurfaces(vector<InitializeTask> tasks, T value);
 
-
-
-
-    };
-
-}
+	};
+} // namespace gpu
 
 //#endif
 

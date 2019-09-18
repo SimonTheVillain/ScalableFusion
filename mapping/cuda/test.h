6e9218d3a59c5d6f39bcdf050dca5058375e9aba
Nikolaus Wagner <nikolaus.wagner@outlook.com>
2019-09-18 12:49:39 +0000

    style: Code cleanup of test

--- mapping/cuda/test.h
+++ mapping/cuda/test.h
@@ -2,24 +2,12 @@
 #define FILE_TEST_H
 
 #include <cuda.h>
-
 #include <cublas.h>
-
 #include <opencv2/opencv.hpp>
-//#include <opencv2/cudaimgproc.hpp>
-//#include <opencv2/cudawarping.hpp>
 
 
 //TODO: small code example to copy (opengl) bindless texture to cv::cuda::GpuMat
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
-{
-   if (code != cudaSuccess)
-   {
-      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-      if (abort) assert(false);
-   }
-}
 
-void copy(cudaTextureObject_t texture,cv::cuda::GpuMat &to);
+void copy(cudaTextureObject_t texture, cv::cuda::GpuMat &to);
+
 #endif

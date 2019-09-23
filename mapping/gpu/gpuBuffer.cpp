29a84396be8815e0784a7116c26d92a299eac306
Nikolaus Wagner <nikolaus.wagner@outlook.com>
2019-09-23 14:16:56 +0000

    style: Code cleanup of gpuBufer

--- mapping/gpu/gpuBuffer.cpp
+++ mapping/gpu/gpuBuffer.cpp
@@ -1,4 +1,5 @@
 #include "gpuBuffer.h"
+
 #include <iostream>
 #include <assert.h>
 
@@ -6,358 +7,286 @@
 
 #include "../cuda/gpuErrchk.h"
 
-
 using namespace std;
 
 template<typename T>
-GpuBuffer<T>::GpuBuffer(size_t elementCount,GLuint bufferType, bool debugNoGpu)
-{
-    this->debugAbsoluteSize=elementCount;
-    debug = debugNoGpu;
-    if(!debugNoGpu){
-    //create opengl buffer and connect it to cuda:
+GpuBuffer<T>::GpuBuffer(size_t element_count, GLuint buffer_type, 
+                        bool debug_no_gpu) {
+	debug_absolute_size_ = element_count;
+	debug_ = debug_no_gpu;
 
+	if(!debug_no_gpu) {
+		//create opengl buffer and connect it to cuda:
+		glGenBuffers(1, &gl_buffer_);
+		glBindBuffer(buffer_type, gl_buffer_);
+		glBufferData(buffer_type, sizeof(T) * element_count, 0, GL_DYNAMIC_DRAW);
 
-    glGenBuffers(1,&glBuffer);
-    glBindBuffer(bufferType,glBuffer);
-    glBufferData(bufferType,sizeof(T)*elementCount,0,GL_DYNAMIC_DRAW);
+		cudaError_t error_test;
+		error_test = cudaGraphicsGLRegisterBuffer(&cuda_graphics_resource_, 
+		                                          gl_buffer_,
+		                                          cudaGraphicsMapFlagsNone);//TODO: test these
+		gpuErrchk(error_test);
+		error_test = cudaGraphicsMapResources(1, &cuda_graphics_resource_);//stream 0
+		gpuErrchk(error_test);
+		size_t size;
+		error_test = cudaGraphicsResourceGetMappedPointer(
+				(void**) &cuda_resource_ptr_,
+				&size,cuda_graphics_resource_);
+		gpuErrchk(error_test);
+		error_test = cudaGraphicsUnmapResources(1, &cuda_graphics_resource_);//stream0
+		gpuErrchk(error_test);
+		//create a list with occupied slots
+		//m_texPosBufferSlotOccupants = new weak_ptr<MeshTexture>[m_maxNrLoadedTextures];
 
-    cudaError_t error_test;
-    error_test = cudaGraphicsGLRegisterBuffer(&cudaGraphicsResource,glBuffer,
-                                              cudaGraphicsMapFlagsNone);//TODO: test these
-    gpuErrchk( error_test );
-    error_test = cudaGraphicsMapResources(1,&cudaGraphicsResource);//stream 0
-    gpuErrchk( error_test );
-    size_t size;
-    error_test = cudaGraphicsResourceGetMappedPointer((void**)&cudaResourcePtr,
-                                                      &size,cudaGraphicsResource);
-    gpuErrchk( error_test );
-    error_test = cudaGraphicsUnmapResources(1,&cudaGraphicsResource);//stream0
-    gpuErrchk( error_test );
-    //create a list with occupied slots
-    //m_texPosBufferSlotOccupants = new weak_ptr<MeshTexture>[m_maxNrLoadedTextures];
+		error_test = cudaGraphicsMapResources(1, &cuda_graphics_resource_);//stream 0
+		gpuErrchk(error_test);
+	}
 
-    error_test = cudaGraphicsMapResources(1,&cudaGraphicsResource);//stream 0
-    gpuErrchk( error_test );
+	//fill the structures with one giant free block
+	free_slots_[element_count].insert(0);
+	starts_[0] = element_count;
+	ends_[element_count - 1] = element_count;
+}
 
-    }
+template<typename T>
+GpuBuffer<T>::~GpuBuffer() {
+	cudaError_t error_test;
 
-
-    //fill the structures with one giant free block
-    freeSlots[elementCount].insert(0);
-    starts[0]=elementCount;
-    ends[elementCount-1]=elementCount;
-
-
-
+	error_test = cudaGraphicsUnmapResources(1, &cuda_graphics_resource_);
+	gpuErrchk(error_test);
+	error_test = cudaGraphicsUnregisterResource(cuda_graphics_resource_);
+	gpuErrchk(error_test);
+	glDeleteBuffers(1, &gl_buffer_);
 }
 
 
 template<typename T>
-GpuBuffer<T>::~GpuBuffer()
-{
-    cudaError_t error_test;
+shared_ptr<GpuBufferConnector<T>> GpuBuffer<T>::getBlock(size_t element_count) {
 
-    error_test = cudaGraphicsUnmapResources(1,&cudaGraphicsResource);
-    gpuErrchk( error_test );
-    error_test = cudaGraphicsUnregisterResource(cudaGraphicsResource);
-    gpuErrchk( error_test );
-    glDeleteBuffers(1,&glBuffer);
+	if(element_count == 0) {
+		cout << "hey maybe this is killing everything" << endl;
+		shared_ptr<GpuBufferConnector<T>> buf(new GpuBufferConnector<T>(this, 0, 0));
+		return buf;
+		assert(0);
+	}
+	mutex_.lock();
+
+	//first check if there are blocks of the requested size
+	map<size_t, set<uint32_t>>::iterator it =
+			free_slots_.lower_bound(element_count);
+
+	if(it == free_slots_.end()) {
+		shared_ptr<GpuBufferConnector<T>> empty;
+		mutex_.unlock();
+		cout << "Error: Ran out of GPU memory for geometry" << endl;
+		assert(0);//No free elements. everything from here on will be pain
+		return empty;
+	}
+
+	//list of free blocks of this size
+	set<uint32_t> &list_free_of_size = it->second;
+	//size of these blocks
+	uint32_t size_of_free = it->first;
+
+	//pointer to the first and last element of the chosen block
+	uint32_t first = *list_free_of_size.begin();
+	uint32_t last = first + size_of_free - 1;
+
+	//index, the adress of the block within the memory
+	uint32_t index = last - element_count + 1;
+
+	//delete the ending points from the lists:
+	if(starts_.find(first) == starts_.end()) {
+		cout << "this should not happen, how did it happen then?" << endl;
+		assert(0);
+	}
+	starts_.erase(starts_.find(first));
+	ends_.erase(ends_.find(last));
+	list_free_of_size.erase(list_free_of_size.begin());
+	if(list_free_of_size.empty()) {
+		//if the list of free slots of this size is empty we also delete
+		//this element in the higher level map
+		free_slots_.erase(it);
+	}
+
+	if(index != first){// if the block is not exactly the requested size
+		//but also create new entries for the object we split apart
+		// (if necessary) and we are not taking a full block
+		uint32_t new_size = size_of_free - element_count;
+		starts_[first] = new_size;
+		ends_[first + new_size - 1] = new_size;
+		free_slots_[new_size].insert(first);
+	}
+
+	shared_ptr<GpuBufferConnector<T>> buffer(new GpuBufferConnector<T>(
+			this, index, element_count));
+	mutex_.unlock();
+	return buffer;
 }
 
-
+// TODO: HEY! THIS SHOUTS FOR RECURSION!!!!!!
 template<typename T>
-std::shared_ptr<GpuBufferConnector<T>> GpuBuffer<T>::getBlock(size_t elementCount)
-{
+void GpuBuffer<T>::rejoinBlock(GpuBufferConnector<T> *block, bool first_level) {
+	if(block->getSize() == 0) {
+		cout << "why does this happen? this really should never ever happen" << endl;
+		return;
+		assert(0);
+	}
+	if(first_level) {
+		mutex_.lock();
+	}
+	uint32_t size = block->size_;
+	uint32_t first = block->index_;
+	uint32_t last = first + size - 1;
+	//check if there is a starting or endpoint overlapping with this
+	//in case stitch it together
+	map<uint32_t, uint32_t>::iterator it_start = starts_.find(last + 1);
+	map<uint32_t, uint32_t>::iterator it_end = ends_.find(first - 1);
 
-    if(elementCount==0){
-        cout << "hey maybe this is killing everything" << endl;
-        std::shared_ptr<GpuBufferConnector<T>> buf(new GpuBufferConnector<T>(this,0,0));
-        return buf;
-        assert(0);
-    }
-    mutex.lock();
+	if(it_start != starts_.end()) {
+		//there is a free block directly after the one we want to insert:
+		//increase the size of our current block
+		uint32_t other_size = it_start->second;
+		uint32_t other_start = last + 1;
+		block->size_ += other_size;
 
-    //first check if there are blocks of the requested size
-    std::map<size_t,std::set<uint32_t>>::iterator it =
-        freeSlots.lower_bound(elementCount);
+		// now erase everything that leads up to this
+		map<size_t, set<uint32_t>>::iterator it = free_slots_.find(other_size);
+		if(it == free_slots_.end()) {
+			assert(0);//this should never happen
+			// if the structure were consistent there
+			//has to be something found in here
+		}
 
-    if(it == freeSlots.end()){
-        std::shared_ptr<GpuBufferConnector<T>> empty;
-        mutex.unlock();
-        std::cout << "Error: Ran out of GPU memory for geometry" << endl;
-        assert(0);//No free elements. everything from here on will be pain
-        return empty;
-    }
+		set<uint32_t> &list_free_of_size = it->second;
 
-    //list of free blocks of this size
-    set<uint32_t> &listFreeOfSize =
-            it->second;
-    //size of these blocks
-    uint32_t sizeOfFree = it->first;
+		if(list_free_of_size.find(other_start) == list_free_of_size.end()) {
+			assert(0);
+		}
+		list_free_of_size.erase(list_free_of_size.find(other_start));
+		if(list_free_of_size.empty()) {
+			free_slots_.erase(it);
+		}
 
-    //pointer to the first and last element of the chosen block
-    uint32_t first = *listFreeOfSize.begin();
-    uint32_t last = first + sizeOfFree - 1;
+		starts_.erase(it_start);
+		ends_.erase(ends_.find(other_start + other_size - 1));
 
-    //index, the adress of the block within the memory
-    uint32_t index =
-        last - elementCount + 1;
+		//recursively call this function to reintroduce the block
+		rejoinBlock(block, false);
 
-    //delete the ending points from the lists:
-    if(starts.find(first) == starts.end()){
-        cout << "this should not happen, how did it happen then?" << endl;
-        assert(0);
-    }
-    starts.erase(starts.find(first));
-    ends.erase(ends.find(last));
-    listFreeOfSize.erase(listFreeOfSize.begin());
-    if(listFreeOfSize.empty()){
-        //if the list of free slots of this size is empty we also delete
-        //this element in the higher level map
-        freeSlots.erase(it);
-    }
+		return;
+	}
 
-    if(index != first){// if the block is not exactly the requested size
+	if(it_end != ends_.end()) {
+		//there is a free block directly before the one we want to insert:
+		//increase the size of our current block+
+		//adapt the index
+		uint32_t other_size = it_end->second;
+		uint32_t other_start = first - other_size;
+		block->index_ -= other_size;
+		block->size_ += other_size;
 
-        //but also create new entries for the object we split apart
-        // (if necessary) and we are not taking a full block
-        uint32_t newSize=sizeOfFree-elementCount;
-        starts[first] = newSize;
-        ends[first+newSize-1] = newSize;
-        freeSlots[newSize].insert(first);
-    }
+		// now erase everything that leads up to this
+		map<size_t, set<uint32_t>>::iterator it = free_slots_.find(other_size);
+		if(it == free_slots_.end()) {
+			assert(0);//this should never happen
+			// if the structure were consistent there
+			//has to be something found in here
+		}
 
+		set<uint32_t> &list_free_of_size = it->second;
+		if(list_free_of_size.find(other_start) == list_free_of_size.end()) {
+			assert(0);//this should never happen either
+		}
+		list_free_of_size.erase(list_free_of_size.find(other_start));
+		if(list_free_of_size.empty()) {
+			free_slots_.erase(it);
+		}
 
+		starts_.erase(starts_.find(other_start));
+		ends_.erase(it_end);
 
-    std::shared_ptr<GpuBufferConnector<T>>
-            buffer(new GpuBufferConnector<T>(this,index,elementCount));
-    mutex.unlock();
-    return buffer;
-}
+		//recursively call this function to reintroduce the block
+		rejoinBlock(block, false);
 
-//HEY! THIS SHOUTS FOR RECURSION!!!!!!
+		return;
+	}
 
-template<typename T>
-void GpuBuffer<T>::rejoinBlock(GpuBufferConnector<T> *block, bool firstLevel)
-{
-    if(block->getSize()==0){
-        cout << "why does this happen? this really should never ever happen" << endl;
-        return;
-        assert(0);
-    }
-    if(firstLevel){
-        mutex.lock();
-    }
-    uint32_t size = block->size;
-    uint32_t first = block->index;
-    uint32_t last = first+size-1;
-    //check if there is a starting or endpoint overlapping with this
-    //in case stitch it together
-    std::map<uint32_t,uint32_t>::iterator itStart =
-            starts.find(last+1);
-    std::map<uint32_t,uint32_t>::iterator itEnd =
-            ends.find(first-1);
+	//since we found everything we
+	starts_[first] = size;
+	ends_[last] = size;
+	free_slots_[size].insert(first);
 
-
-    if(itStart != starts.end()){
-        //there is a free block directly after the one we want to insert:
-        //increase the size of our current block
-        uint32_t otherSize = itStart->second;
-        uint32_t otherStart = last+1;
-        block->size += otherSize;
-
-        // now erase everything that leads up to this
-        std::map<size_t,std::set<uint32_t>>::iterator it =
-            freeSlots.find(otherSize);
-        if(it == freeSlots.end()){
-            assert(0);//this should never happen
-            // if the structure were consistent there
-            //has to be something found in here
-        }
-
-        set<uint32_t> &listFreeOfSize =
-                it->second;
-
-        if(listFreeOfSize.find(otherStart) == listFreeOfSize.end()){
-            assert(0);
-        }
-        listFreeOfSize.erase(listFreeOfSize.find(otherStart));
-        if(listFreeOfSize.empty()){
-            freeSlots.erase(it);
-        }
-
-        starts.erase(itStart);
-        ends.erase(ends.find(otherStart + otherSize - 1));
-
-        //recursively call this function to reintroduce the block
-        rejoinBlock(block, false);
-
-        return;
-    }
-    if(itEnd != ends.end()){
-        //there is a free block directly before the one we want to insert:
-        //increase the size of our current block+
-        //adapt the index
-        uint32_t otherSize = itEnd->second;
-        uint32_t otherStart = first - otherSize;
-        block->index -= otherSize;
-        block->size += otherSize;
-
-        // now erase everything that leads up to this
-        std::map<size_t,std::set<uint32_t>>::iterator it =
-            freeSlots.find(otherSize);
-        if(it == freeSlots.end()){
-            assert(0);//this should never happen
-            // if the structure were consistent there
-            //has to be something found in here
-        }
-
-        set<uint32_t> &listFreeOfSize =
-                it->second;
-        if(listFreeOfSize.find(otherStart) == listFreeOfSize.end()){
-            assert(0);//this should never happen either
-        }
-        listFreeOfSize.erase(listFreeOfSize.find(otherStart));
-        if(listFreeOfSize.empty()){
-            freeSlots.erase(it);
-        }
-
-        starts.erase(starts.find(otherStart));
-        ends.erase(itEnd);
-
-        //recursively call this function to reintroduce the block
-        rejoinBlock(block, false);
-
-        return;
-    }
-
-    //since we found everything we
-    starts[first] = size;
-    ends[last] = size;
-    freeSlots[size].insert(first);
-
-
-
-    mutex.unlock();
+	mutex_.unlock();
 }
 
 template<typename T>
-void GpuBuffer<T>::upload(T *data, uint32_t index, uint32_t count)
-{
-    cudaError_t error_test;
-    error_test = cudaMemcpy( &(cudaResourcePtr[index]),
-                data,
-                sizeof(T)*count,
-                cudaMemcpyHostToDevice);
-    gpuErrchk( error_test );
-
+void GpuBuffer<T>::upload(T *data, uint32_t index, uint32_t count) {
+	cudaError_t error_test;
+	error_test = cudaMemcpy(&(cuda_resource_ptr_[index]), data, sizeof(T) * count,
+	                        cudaMemcpyHostToDevice);
+	gpuErrchk(error_test);
 }
 
 template<typename T>
-void GpuBuffer<T>::download(T *data, uint32_t index, uint32_t count){
-
-    cudaError_t error_test;
-     error_test = cudaMemcpy( data,
-                 &(cudaResourcePtr[index]),
-                 sizeof(T)*count,
-                 cudaMemcpyDeviceToHost);
-     cudaDeviceSynchronize();
-     gpuErrchk( error_test );
+void GpuBuffer<T>::download(T *data, uint32_t index, uint32_t count) {
+	cudaError_t error_test;
+	error_test = cudaMemcpy(data, &(cuda_resource_ptr_[index]), sizeof(T) * count,
+	                        cudaMemcpyDeviceToHost);
+	 cudaDeviceSynchronize();
+	 gpuErrchk(error_test);
 }
 
 template<typename T>
 uint32_t GpuBuffer<T>::getUsedElements() {
-    mutex.lock();
-    int count = 0;
-    for (auto size : starts) {
-        count += size.second;
-    }
-
-
-    mutex.unlock();
-    return debugAbsoluteSize - count;
+	mutex_.lock();
+	int count = 0;
+	for (auto size : starts_) {
+		count += size.second;
+	}
+	mutex_.unlock();
+	return debug_absolute_size_ - count;
 }
 
 template<typename T>
-uint32_t GpuBuffer<T>::getFreeElements(){
-
-    mutex.lock();
-    int count = 0;
-    for (auto size : starts) {
-        count += size.second;
-    }
-
-
-    mutex.unlock();
-    return count;
-}
-
-
-template<typename T>
-GpuBufferConnector<T>::GpuBufferConnector(GpuBuffer<T> *buffer,
-                                          uint32_t index,
-                                          uint32_t size):
-    buffer(buffer),
-    index(index),
-    size(size){
-
+uint32_t GpuBuffer<T>::getFreeElements() {
+	mutex_.lock();
+	int count = 0;
+	for (auto size : starts_) {
+		count += size.second;
+	}
+	mutex_.unlock();
+	return count;
 }
 
 template<typename T>
-GpuBufferConnector<T>::~GpuBufferConnector()
-{
-    buffer->rejoinBlock(this);
+GpuBufferConnector<T>::GpuBufferConnector(GpuBuffer<T> *buffer, uint32_t index,
+                                          uint32_t size)
+		: buffer_(buffer),
+		  index_(index),
+		  size_(size) {
 }
 
 template<typename T>
-void GpuBufferConnector<T>::upload(T *data, uint32_t offset, uint32_t count)
-{
-    if(count==0){
-        return;
-    }
-    buffer->upload(data,index + offset,count);
+GpuBufferConnector<T>::~GpuBufferConnector() {
+	buffer_->rejoinBlock(this);
 }
 
 template<typename T>
-void GpuBufferConnector<T>::download(T *data, uint32_t offset, uint32_t count)
-{
-    if(count==0){
-        return;
-    }
-    buffer->download(data,index + offset,count);
+void GpuBufferConnector<T>::upload(T *data, uint32_t offset, uint32_t count) {
+	if(count == 0) {
+		return;
+	}
+	buffer_->upload(data, index_ + offset, count);
 }
 
-/*
-void testBuffer()
-{
-
-    GpuBuffer<uint8_t> buffer(100,true);
-
-    std::shared_ptr<GpuBufferConnector<uint8_t>> blockA = buffer.getBlock(10);
-    std::shared_ptr<GpuBufferConnector<uint8_t>> blockC = buffer.getBlock(10);
-    blockA.reset();
-
-    std::shared_ptr<GpuBufferConnector<uint8_t>> blockD = buffer.getBlock(9);
-
-    std::shared_ptr<GpuBufferConnector<uint8_t>> blockE = buffer.getBlock(1);
-
-
-    blockC.reset();
-
-
-    std::shared_ptr<GpuBufferConnector<uint8_t>> blockB = buffer.getBlock(100);
-
-
-    blockB.reset();
-    blockD.reset();
-    blockE.reset();
-
-    cout << "how is the state of this buffer? " << endl;
-
+template<typename T>
+void GpuBufferConnector<T>::download(T *data, uint32_t offset, uint32_t count) {
+	if(count == 0){
+		return;
+	}
+	buffer_->download(data, index_ + offset, count);
 }
-*/
-
 
 template class GpuBufferConnector<GpuVertex>;
 template class GpuBufferConnector<Eigen::Vector2f>;
--- mapping/gpu/gpuBuffer.h
+++ mapping/gpu/gpuBuffer.h
@@ -8,113 +8,120 @@
 #include <map>
 #include <utility>
 
-
 #include <GL/glew.h>
-
 #include <cuda.h>
 #include <cuda_gl_interop.h>
 
 #include "../cuda/gpuMeshStructure.h"
 
-
+using namespace std;
 
 template<typename T>
 class GpuBuffer;
 
-//TODO: get rid of this??????? ( i don't understand the need for this anymore
-class GpuBufferConnectorBase{
-/* this only exists so we can store
- * shared_ptr to this without it being deleted unintentionally!
- * */
-};
-
-template<typename T>
-
 //TODO: rename BufferConnector to bufferHandle / slot? (except for when it really is more a connector than a slot
-class GpuBufferConnector{// : public GpuBufferConnectorBase{
-    friend GpuBuffer<T>;
-private:
-    uint32_t index;//the starting index for this slot
-    uint32_t size;//size
+template<typename T>
+class GpuBufferConnector {
+	friend GpuBuffer<T>;
 
-    GpuBuffer<T>* buffer=0;
-
-    GpuBufferConnector(GpuBuffer<T>* buffer,uint32_t index, uint32_t size);
 public:
 
+	~GpuBufferConnector();
 
-    ~GpuBufferConnector();
+	uint32_t getStartingIndex() {
+		return index_;
+	}
 
-    uint32_t getStartingIndex(){return index;}
+	T* getStartingPtr() {
+		return &(buffer_->getCudaPtr()[index_]);
+	}
 
-    T* getStartingPtr(){return &(buffer->getCudaPtr()[index]);}
+	void upload(T *data, uint32_t offset, uint32_t count);
 
+	void upload(T *data) {
+		upload(data, 0, size_);
+	}
 
-    void upload(T *data,uint32_t offset, uint32_t count);
-    void download(T* data,uint32_t offset, uint32_t count);
+	void download(T *data, uint32_t offset, uint32_t count);
 
+	void download(T *data) {
+		download(data, 0, size_);
+	}
 
-    void upload(T* data){
-        upload(data,0,size);
-    }
+	uint32_t getSize() {
+		return size_;
+	}
 
-    void download(T* data){
-        download(data,0,size);
-    }
-    uint32_t getSize(){return size;}
+private:
 
+	GpuBufferConnector(GpuBuffer<T> *buffer, uint32_t index, uint32_t size);
+
+	uint32_t index_;//the starting index for this slot
+	uint32_t size_;
+
+	GpuBuffer<T> *buffer_ = 0;
 
 };
 
 //TODO: rename this to GpuBufferCollection
 template<typename T>
-class GpuBuffer{
-private:
-    std::mutex mutex;
-
-    //store the free slots of a certain size
-    //first its only one, of full size, smaller ones get split off of this one
-    //the free slot gets moved down the according size
-    //problems:
-    //1) giving back one element and connecting this whith existing slots
-    //probably doable with start and end map!
-    //2) finding the next best slot (next biggest key)
-    // maps are sorted
-    // https://stackoverflow.com/questions/1660195/c-how-to-find-the-biggest-key-in-a-stdmap
-
-
-    //key: size, value: adress (maybe replace this vector with a set(sorted))
-    std::map<size_t,std::set<uint32_t>> freeSlots;
-
-    //key: starting index, value: size
-    std::map<uint32_t,uint32_t> starts;
-
-    //key: ending index, value: size
-    std::map<uint32_t,uint32_t> ends;
-
-    GLuint glBuffer;
-    cudaGraphicsResource_t cudaGraphicsResource;
-    T* cudaResourcePtr;
-    bool debug=false;
-    size_t debugAbsoluteSize;//debug because it is unused
+class GpuBuffer {
 public:
-    GpuBuffer(size_t elementCount,GLuint bufferType = GL_ARRAY_BUFFER,bool debugNoGpu=false);
-    ~GpuBuffer();
 
+	GpuBuffer(size_t element_count, GLuint buffer_type = GL_ARRAY_BUFFER,
+	          bool debug_no_gpu = false);
 
-    std::shared_ptr<GpuBufferConnector<T>> getBlock(size_t elementCount);
+	~GpuBuffer();
 
-    void rejoinBlock(GpuBufferConnector<T> *block, bool firstLevel = true);
+	shared_ptr<GpuBufferConnector<T>> getBlock(size_t element_count);
 
+	void rejoinBlock(GpuBufferConnector<T> *block, bool first_level = true);
 
-    void upload(T* data, uint32_t index, uint32_t count);
-    void download(T* data,uint32_t index, uint32_t count);
-    GLuint getGlName(){return glBuffer;}
-    T* getCudaPtr(){return cudaResourcePtr;}
+	void upload(T *data, uint32_t index, uint32_t count);
 
+	void download(T *data, uint32_t index, uint32_t count);
 
-    uint32_t getUsedElements();
-    uint32_t getFreeElements();
+	GLuint getGlName() {
+		return gl_buffer_;
+	}
+
+	T *getCudaPtr() {
+		return cuda_resource_ptr_;
+	}
+
+	uint32_t getUsedElements();
+
+	uint32_t getFreeElements();
+
+private:
+
+	mutex mutex_;
+
+	//store the free slots of a certain size
+	//first its only one, of full size, smaller ones get split off of this one
+	//the free slot gets moved down the according size
+	//problems:
+	//1) giving back one element and connecting this whith existing slots
+	//probably doable with start and end map!
+	//2) finding the next best slot (next biggest key)
+	// maps are sorted
+	// https://stackoverflow.com/questions/1660195/c-how-to-find-the-biggest-key-in-a-stdmap
+
+	//key: size, value: adress (maybe replace this vector with a set(sorted))
+	map<size_t, set<uint32_t>> free_slots_;
+
+	//key: starting index, value: size
+	map<uint32_t, uint32_t> starts_;
+
+	//key: ending index, value: size
+	map<uint32_t, uint32_t> ends_;
+
+	GLuint gl_buffer_;
+	cudaGraphicsResource_t cuda_graphics_resource_;
+	T *cuda_resource_ptr_;
+	bool debug_ = false;
+	size_t debug_absolute_size_;//debug because it is unused
+
 };
 
 typedef GpuBuffer<GpuVertex> VertexBuffer;

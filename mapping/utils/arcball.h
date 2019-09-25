719d4dbdaad2500fa68a40efcc6aea908d61d011
Nikolaus Wagner <nikolaus.wagner@outlook.com>
2019-09-25 14:17:08 +0000

    style: Code cleanup of arcball

--- mapping/utils/arcball.h
+++ mapping/utils/arcball.h
@@ -1,79 +1,72 @@
 #ifndef FILE_ARCBALL_H
 #define FILE_ARCBALL_H
 
-
-
-
 //https://en.wikibooks.org/wiki/OpenGL_Programming/Modern_OpenGL_Tutorial_Arcball
 #include <Eigen/Core>
 //angle axis rotation
 #include <Eigen/Geometry>
 
-
+using namespace std;
 using namespace Eigen;
 
-class Arcball{
-private:
-    float m_last_x=0;
-    float m_last_y=0;
-    float m_cur_x=0;
-    float m_cur_y=0;
-    int m_width=800;
-    int m_height=600;
-
-    Vector3f last;
-
-    Vector3f getArcballVec(int x,int y){
-        //normalized point on screen (y is inverted)
-        Vector3f P(1.0*x/m_width*2-1.0,-(1.0*y/m_height*2-1.0),0);
-        float length_2=P[0]*P[0]+P[1]*P[1];
-        if(length_2<1.0){
-            P[2]=std::sqrt(1.0f-length_2);
-        }else{
-            P=P.normalized();
-        }
-        length_2=P[0]*P[0]+P[1]*P[1]+P[2]*P[2];
-        return P;
-    }
-
-    Matrix4f view;
+class Arcball {
 public:
 
-    Arcball(){
-        view=Matrix4f::Identity();
+	Arcball()
+			: view_(Matrix4f::Identity()) {
+	}
 
-    }
-    ~Arcball(){
+	void setFramebufferData(int width, int height) {
+		width_ = width;
+		height_ = height;
+	}
 
-    }
-    void setFramebufferData(int width,int height){
-        m_width=width;
-        m_height=height;
+	void clickBegin(float x,float y) {
+		last_x_ = x;
+		last_y_ = y;
+		last_ = getArcballVec_(x, y);
+	}
 
-    }
+	void drag(float x, float y) {
+		Vector3f current = getArcballVec_(x, y);
+		float angle = acos(min(1.0f, current.dot(last_)));
+		Vector3f axis = current.cross(last_);
+		last_ = current;
+		Matrix4f m;
+		m = Matrix4f::Identity();
+		m.block<3, 3>(0, 0) = AngleAxisf(angle, 
+		                                 axis.normalized()).toRotationMatrix();
+		view_ = m * view_;
+	}
 
-    void clickBegin(float x,float y){
-        m_last_x=x;
-        m_last_y=y;
-        last=getArcballVec(x,y);
+	Matrix4f getView() {
+		return view_;
+	}
 
-    }
+private:
 
-    void drag(float x,float y){
-        Vector3f current=getArcballVec(x,y);
-        float angle = std::acos(std::min(1.0f,current.dot(last)));
-        Vector3f axis=current.cross(last);
-        last=current;
-        Matrix4f m;
-        m=Matrix4f::Identity();
-        m.block<3,3>(0,0)=AngleAxisf(angle,axis.normalized()).toRotationMatrix();
-        view=m*view;
+	float last_x_ = 0;
+	float last_y_ = 0;
+	int width_ = 800;
+	int height_ = 600;
 
-    }
+	Vector3f last_;
 
-    Matrix4f getView(){
-        return view;
-    }
-
+	Vector3f getArcballVec_(int x, int y) {
+		//normalized point on screen (y is inverted)
+		Vector3f P(  1.0 * x / width_  * 2 - 1.0, 
+		           -(1.0 * y / height_ * 2 - 1.0), 0);
+		float length_2 = P[0] * P[0] + P[1] * P[1];
+		if(length_2 < 1.0) {
+			P[2] = sqrt(1.0f - length_2);
+		} else {
+			P = P.normalized();
+		}
+		length_2 = P[0] * P[0] + P[1] * P[1] + P[2] * P[2];
+		return P;
+	}
+	Matrix4f view_;
+	
 };
+
 #endif

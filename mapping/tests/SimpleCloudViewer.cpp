959150108a2c7481b1b51f7914537e3e5f8b17a4
Nikolaus Wagner <nikolaus.wagner@outlook.com>
2019-09-25 13:45:32 +0000

    style: Code cleanup of simple cloud viewer

--- mapping/tests/SimpleCloudViewer.cpp
+++ mapping/tests/SimpleCloudViewer.cpp
@@ -1,309 +1,238 @@
-//
-// Created by simon on 3/28/19.
-//
-
 // Example program
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <Eigen/Eigen>
 
+#include <Eigen/Eigen>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-
-
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
 
 #include <camera.h>
 #include "../utils/arcball.h"
 #include "../rendering/renderableModel.h"
-
 #include "../graph/DeformationGraph.h"
 
-
 #define MAX_LABELS 21
-
-
 
 using namespace std;
 using namespace Eigen;
 
-
-
 static Arcball arcball;
 
-
 //TODO: get coordinates
-static float distFromBall=3.0f;
+static float dist_from_ball = 3.0f;
 static Vector3f trans;
+static bool capture_left = false;
+static bool capture_right = false;
+static double xpos_old = 0, ypos_old = 0;
+static void cursor_position_callback(GLFWwindow *window, double xpos, 
+                                     double ypos) {
+	(void) window;
 
-static bool captureLeft=false;
-static bool captureRight=false;
-static double xposOld=0,yposOld=0;
-static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
-{
-    (void)window;
-
-    float dx=static_cast<float>(xposOld-xpos);
-    float dy=static_cast<float>(yposOld-ypos);
-    if(captureLeft){
-        float speed=0.05f;
-        //trans+=Vector3f(-dx*speed,dy*speed,0);
-        Vector4f trans4(-dx*speed,dy*speed,0,1);
-        trans4=arcball.getView().inverse()*trans4;
-        trans+=trans4.block<3,1>(0,0);
-        //cout << trans << endl;
-    }
-    if(captureRight){
-        //do the arcball thingy with the right bouse button
-        arcball.drag(xpos,ypos);
-    }
-    xposOld=xpos;
-    yposOld=ypos;
+	float dx = static_cast<float>(xpos_old - xpos);
+	float dy = static_cast<float>(ypos_old - ypos);
+	if(capture_left) {
+		float speed = 0.05f;
+		Vector4f trans4(-dx * speed, dy * speed, 0, 1);
+		trans4 = arcball.getView().inverse() * trans4;
+		trans += trans4.block<3, 1>(0, 0);
+	}
+	if(capture_right) {
+		//do the arcball thingy with the right bouse button
+		arcball.drag(xpos, ypos);
+	}
+	xpos_old = xpos;
+	ypos_old = ypos;
 }
 
-
-
-static bool readOutSurfaceInfo=false;
-static bool centerCamera = false;
-void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
-{
-    (void)mods;
-    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS){
-        captureLeft=true;
-    }
-    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_RELEASE){
-        captureLeft=false;
-    }
-
-    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS){
-        captureRight=true;
-        arcball.clickBegin(static_cast<float>(xposOld),
-                           static_cast<float>(yposOld));
-    }
-    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE){
-        captureRight=false;
-    }
-    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS &&
-        glfwGetKey(window,GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS){
-        readOutSurfaceInfo = true;
-    }
-
-    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS &&
-        glfwGetKey(window,GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS){
-        centerCamera = true;
-    }
-}
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-{
-    (void)window;
-    (void)xoffset;
-    //cout<< "dist from arcball center " << distFromBall << endl;
-    float scale=0.05f;
-    distFromBall*=(1.0f + static_cast<float>(yoffset)*scale);
+static bool read_out_surface_info = false;
+static bool center_camera = false;
+void mouse_button_callback(GLFWwindow *window, int button, int action, 
+                           int mods) {
+	(void) mods;
+	if(button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS) {
+		capture_left = true;
+	}
+	if(button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_RELEASE) {
+		capture_left = false;
+	}
+	if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+		capture_right = true;
+		arcball.clickBegin(static_cast<float>(xpos_old),
+		                   static_cast<float>(ypos_old));
+	}
+	if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
+		capture_right=false;
+	}
+	if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS &&
+	   glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) {
+		read_out_surface_info = true;
+	}
+	if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS &&
+	   glfwGetKey(window,GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
+		center_camera = true;
+	}
 }
 
-bool nextStep=false;
-void key_callback(GLFWwindow* window,int key, int scancode, int action, int mods){
-    if(key == GLFW_KEY_SPACE && action == GLFW_PRESS){
-        nextStep = true;
-    }
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset) {
+	(void) window;
+	(void) xoffset;
+	float scale = 0.05f;
+	dist_from_ball *= (1.0f + static_cast<float>(yoffset) * scale);
 }
 
+bool next_step = false;
+void key_callback(GLFWwindow *window, int key, int scancode, int action, 
+                  int mods) {
+	if(key == GLFW_KEY_SPACE && action == GLFW_PRESS) {
+		next_step = true;
+	}
+}
 
+struct Intrinsics {
+	int width, height;
+	float cx, cy;
+	float fx, fy;
+	float k1, k2, k3;
+	float p1, p2;
 
-struct Intrinsics{
-    int width,height;
-    float cx,cy;
-    float fx,fy;
-    float k1,k2,k3;
-    float p1,p2;
-
-    void read(string line){
-        sscanf(line.c_str(),"%d , %d , %f , %f , %f , %f , %f , %f ,%f ,%f ,%f",
-                &width,&height,&cx,&cy,&fx,&fy,&k1,&k2,&k3,&p1,&p2);
-    }
+	void read(string line) {
+		sscanf(line.c_str(),"%d , %d , %f , %f , %f , %f , %f , %f ,%f ,%f ,%f",
+		       &width, &height, &cx, &cy, &fx, &fy, &k1, &k2, &k3, &p1, &p2);
+	}
 };
 
-ostream & operator << (ostream &out, const Intrinsics &i)
-{
-    out << "cx " << i.cx << " cy " << i.cy << " fx " << i.fx << " fy " << i.fy ;
-    out << " k1,k2,k3 " << i.k1 << ", " << i.k2 << ", " << i.k3 ;
-    out << " p1,p2 " << i.p1 << ", " << i.p2;
-    return out;
+ostream &operator<<(ostream &out, const Intrinsics &i) {
+	out << "cx " << i.cx << " cy " << i.cy << " fx " << i.fx << " fy " << i.fy ;
+	out << " k1,k2,k3 " << i.k1 << ", " << i.k2 << ", " << i.k3;
+	out << " p1,p2 " << i.p1 << ", " << i.p2;
+	return out;
 }
 
+int main() {
+	string path =
+			"/home/simon/datasets/structure_core/single_shots";
+
+	string extrinsics_file = path + "/extrinsics.txt";
+
+	string intrinsics_file = path + "/intrinsics.txt";
+
+	Matrix4f imu_from_depth;
+	Matrix4f imu_from_color;
+	fstream in;
+	in.open(extrinsics_file);
+	string line;
+	getline(in, line);
+	for(int i = 0; i < 16; i++) {
+		in >> imu_from_depth(i);
+	}
+	cout << imu_from_depth << endl;
+
+	getline(in, line);
+	getline(in, line);
+	for(int i = 0; i < 16; i++) {
+		in >> imu_from_color(i);
+	}
+	cout << imu_from_color << endl;
+	in.close();
+
+	//INTRINSICS:
+	Intrinsics intrinsics_color;
+	Intrinsics intrinsics_depth;
+	Intrinsics intrinsics_ir;
+
+	in.open(intrinsics_file);
+	getline(in, line);
+	intrinsics_color.read(line);
+
+	getline(in, line);
+	intrinsics_ir.read(line);
+
+	getline(in, line);
+	intrinsics_depth.read(line);
+
+	cout << intrinsics_ir << endl;
+	cout << intrinsics_color << endl;
+	cout << intrinsics_depth << endl;
+
+	int image_nr = 1;
+
+	cv::Mat depth;
+	cv::Mat ir;
+	cv::Mat rgb;
+	string im_path = path + "/depth/" + to_string(image_nr) + ".png";
+	depth = cv::imread(im_path);
+
+	im_path = path + "/rgb/" + to_string(image_nr) + ".png";
+	rgb = cv::imread(im_path);
+
+	im_path = path + "/ir/" + to_string(image_nr) + ".png";
+	ir = cv::imread(im_path);
+
+	cv::imshow("rgb", rgb);
+	cv::imshow("ir", ir);
+	cv::imshow("depth", depth * 10);
+	cv::waitKey();
+
+	for(size_t i = 0; i < depth.rows; i++) {
+		for(size_t j = 0; j < depth.cols; j++) {
+			//TODO: all the projection bullshit, transformation and what not
+		}
+	}
+
+	//TODO: use the simple
+	return 0;
+
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
 
 
-int main()
-{
-    std::string path =
-            "/home/simon/datasets/structure_core/single_shots";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	GLFWwindow *window;
+	window = glfwCreateWindow(1280, 1024, "graph", nullptr, nullptr);
 
-    std::string extrinsicsFile =
-            path + "/extrinsics.txt";
+	//set the callback functions for the different inputs
+	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetCursorPosCallback(window, cursor_position_callback);
+	glfwSetMouseButtonCallback(window, mouse_button_callback);
+	glfwSetKeyCallback(window, key_callback);
 
-    std::string intrinsicsFile =
-            path + "/intrinsics.txt";
+	glfwMakeContextCurrent(window);
 
+	glewExperimental = GL_TRUE;
+	glewInit();
+	glGetError();
 
+	while(!glfwWindowShouldClose(window)) {
 
-    Eigen::Matrix4f imuFromDepth;
-    Eigen::Matrix4f imuFromColor;
-    fstream in;
-    in.open(extrinsicsFile);
-    std::string line;
-    std::getline(in,line);
-    for(int i=0;i<16;i++){
+		glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glEnable(GL_DEPTH_TEST);
 
-        in >> imuFromDepth(i);
-    }
-    cout << imuFromDepth << endl;
+		//render:
+		Matrix4f proj = Camera::projection(static_cast<float>(M_PI) * 0.3f,
+		                                   800.0f / 600.0f);
+		Matrix4f translation = Matrix4f::Identity();
+		translation.block<3, 1>(0, 3) = trans;
+		Matrix4f trans_from_arcball = Matrix4f::Identity();
+		trans_from_arcball(2, 3) = -dist_from_ball;
+		Matrix4f mat = proj * trans_from_arcball * arcball.getView() * translation;
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 
+		int width, height;
+		glfwGetWindowSize(window, &width, &height);
+		arcball.setFramebufferData(width, height);
 
-    std::getline(in,line);
-    std::getline(in,line);
-    for(int i=0;i<16;i++){
+		if(next_step) {
+			next_step = false;
+		}
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
 
-        in >> imuFromColor(i);
-    }
-    cout << imuFromColor << endl;
-    in.close();
-
-
-    //INTRINSICS:
-
-    Intrinsics intrinsicsColor;
-    Intrinsics intrinsicsDepth;
-    Intrinsics intrinsicsIr;
-
-    in.open(intrinsicsFile);
-    std::getline(in,line);
-    intrinsicsColor.read(line);
-
-    std::getline(in,line);
-    intrinsicsIr.read(line);
-
-    std::getline(in,line);
-    intrinsicsDepth.read(line);
-
-    cout << intrinsicsIr << endl;
-    cout << intrinsicsColor << endl;
-    cout << intrinsicsDepth << endl;
-
-
-
-    int imageNr = 1;
-
-    cv::Mat depth;
-    cv::Mat ir;
-    cv::Mat rgb;
-    string imPath =
-            path + "/depth/" + std::to_string(imageNr) + ".png";
-    depth = cv::imread(imPath);
-
-
-    imPath =
-            path + "/rgb/" + std::to_string(imageNr) + ".png";
-    rgb = cv::imread(imPath);
-
-
-    imPath =
-            path + "/ir/" + std::to_string(imageNr) + ".png";
-    ir = cv::imread(imPath);
-
-    cv::imshow("rgb",rgb);
-    cv::imshow("ir",ir);
-    cv::imshow("depth",depth * 10);
-    cv::waitKey();
-    //Eigen::Matrix4f colorToDepth =
-    //std::vector<RenderVerySimpleModel::Vertex> vertices;
-    for(size_t i=0;i<depth.rows;i++){
-        for(size_t j=0;j<depth.cols;j++){
-            //TODO: all the projection bullshit, transformation and what not
-        }
-    }
-
-    //TODO: use the simple
-    //RenderVerySimpleModel simpleModel;
-    //simpleModel.primitiveType = GL_POINTS;
-    //simpleModel.setMesh();
-    return 0;
-
-    if (!glfwInit())
-        exit(EXIT_FAILURE);
-
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
-    GLFWwindow* window;
-    window = glfwCreateWindow(1280,1024,"graph", nullptr,nullptr);
-
-
-    //set the callback functions for the different inputs
-    glfwSetScrollCallback(window,scroll_callback);
-    glfwSetCursorPosCallback(window,cursor_position_callback);
-    glfwSetMouseButtonCallback(window,mouse_button_callback);
-    glfwSetKeyCallback(window,key_callback);
-//    glfwSetKeyCallback(window,key_callback);
-
-
-    glfwMakeContextCurrent(window);
-
-    glewExperimental = GL_TRUE;
-    glewInit();
-    glGetError();
-
-
-
-
-
-    while(!glfwWindowShouldClose(window)){
-
-        glClearColor(0.3f,0.3f,0.3f,1.0f);
-        //glClearColor(1.0f,1.0f,1.0f,1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glEnable(GL_DEPTH_TEST);
-
-        //render:
-
-        Matrix4f proj=Camera::projection(static_cast<float>(M_PI)*0.3f,
-                                         800.0f/600.0f);
-        Matrix4f translation=Matrix4f::Identity();
-        translation.block<3,1>(0,3)=trans;
-        //cout << "trans " << trans << endl;
-        //cout << "view " << arcball.getView() << endl;
-        Matrix4f transFromArcball = Matrix4f::Identity();
-        transFromArcball(2,3)=-distFromBall;
-        Eigen::Matrix4f mat = proj * transFromArcball * arcball.getView()*translation;
-        //mat= Matrix4f::Identity();
-        //mat(0,0) = mat(1,1) = mat(2,2)
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-
-        int width, height;
-        glfwGetWindowSize(window, &width, &height);
-        arcball.setFramebufferData(width,height);
-
-
-        if(nextStep){
-            nextStep = false;
-
-        }
-
-
-
-
-    }
-
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
-
-
-
-
-    return 0;
+	return 0;
 }

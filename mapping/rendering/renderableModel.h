03efccc8b74d4a90e4cb87ebb91ed6dde9283733
Nikolaus Wagner <nikolaus.wagner@outlook.com>
2019-09-25 12:49:12 +0000

    style: Code cleanup of renderable model

--- mapping/rendering/renderableModel.h
+++ mapping/rendering/renderableModel.h
@@ -1,81 +1,77 @@
 #ifndef FILE_RENDERABLE_MODEL_H
 #define FILE_RENDERABLE_MODEL_H
 
-#include <Eigen/Eigen>
-#include <shader.h>
 #include <memory>
 
+#include <Eigen/Eigen>
+#include <shader.h>
+
+using namespace std;
+using namespace Eigen;
 
 //TODO: rename this!!!
-class RenderVerySimpleModel{
+class RenderVerySimpleModel {
+public:
+	struct Vertex {
+		Vertex(Vector4f pos, Vector4f color) 
+				: p(pos),
+				  c(color) {
+		}
+		Vector4f p;
+		Vector4f c;
+	};
+
+	RenderVerySimpleModel();
+
+	~RenderVerySimpleModel();
+
+	void setMesh(vector<Vector4f> &vertices, vector<unsigned int> indices);
+
+	void render(Matrix4f &camProj);
+
+	GLuint primitiveType = GL_LINES;
+
+	Matrix4f pose = Matrix4f::Identity();
+	Vector4f color = Vector4f(1, 0, 0, 1);
 
 private:
-    GLuint vertexBuffer = 0;
-    GLuint elementBuffer = 0;
-    int count;
 
-    GLuint VAO = 0;
-    static std::weak_ptr<gfx::GLSLProgram> s_flatProgram;
-    std::shared_ptr<gfx::GLSLProgram> unlitProgram;
+	GLuint vertex_buffer_ = 0;
+	GLuint element_buffer_ = 0;
+	int count_;
 
-
-public:
-    struct Vertex{
-        Eigen::Vector4f p;
-        Eigen::Vector4f c;
-
-        Vertex(Eigen::Vector4f pos,Eigen::Vector4f color){
-            p=pos;
-            c=color;
-        }
-        ~Vertex(){}
-    };
-
-    RenderVerySimpleModel();
-    ~RenderVerySimpleModel();
-
-
-    GLuint primitiveType = GL_LINES;
-    void setMesh(std::vector<Eigen::Vector4f> &vertices, std::vector<unsigned int> indices);
-
-
-    Eigen::Matrix4f pose = Eigen::Matrix4f::Identity();
-    Eigen::Vector4f color = Eigen::Vector4f(1,0,0,1);
-    void render(Eigen::Matrix4f &camProj);
+	GLuint VAO_ = 0;
+	static weak_ptr<gfx::GLSLProgram> s_flat_program_;
+	shared_ptr<gfx::GLSLProgram> unlit_program_;
 };
 
-
-
-class CameraFrustrumRenderableModel : public RenderVerySimpleModel{
-private:
-    //RenderVerySimpleModel model;
+class CameraFrustrumRenderableModel : public RenderVerySimpleModel {
 public:
-    CameraFrustrumRenderableModel(Eigen::Vector4f color, Eigen::Vector4f intrinsics, Eigen::Vector2f resolution,
-                                  float nearClippingPlane, float farClippingPlane);
+	CameraFrustrumRenderableModel(Vector4f color, Vector4f intrinsics, 
+	                              Vector2f resolution, float near_clipping_plane, 
+	                              float far_clipping_plane);
 
-
-    ~CameraFrustrumRenderableModel();
+	~CameraFrustrumRenderableModel() {
+	}
 };
 
+class WireSphereModel : public RenderVerySimpleModel {
+public:
 
-class WireSphereModel : public RenderVerySimpleModel{
+	WireSphereModel(Vector4f color, Vector4f pos, float radius);
+
+	void setPosition(Vector3f position);
+
+	void setRadius(float radius);
+
 //TODO: implement this!
 private:
 
-    Eigen::Vector3f color;
-    Eigen::Vector3f pos;
-    Eigen::Vector3f scale;
+	Vector3f color_;
+	Vector3f pos_;
+	Vector3f scale_;
 
-    void updatePose();
-public:
-    WireSphereModel(Eigen::Vector4f color, Eigen::Vector4f pos,float radius);
-    ~WireSphereModel();
-
-    void setPosition(Eigen::Vector3f position);
-    void setRadius(float radius);
-
-
-
+	void updatePose_();
 
 };
 

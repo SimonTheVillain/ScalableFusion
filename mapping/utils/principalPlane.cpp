07897046ea2f25dfbcbcba8857f43fb099445b17
Nikolaus Wagner <nikolaus.wagner@outlook.com>
2019-09-30 15:10:00 +0000

    style: Code cleanup of principalPlane

--- mapping/utils/principalPlane.cpp
+++ mapping/utils/principalPlane.cpp
@@ -2,58 +2,47 @@
 
 using namespace Eigen;
 
-PrincipalPlaneAccumulator::PrincipalPlaneAccumulator()
-{
-    clear();
+PrincipalPlaneAccumulator::PrincipalPlaneAccumulator() {
+	clear();
 }
 
-PrincipalPlaneAccumulator::~PrincipalPlaneAccumulator()
-{
-
+void PrincipalPlaneAccumulator::clear() {
+	count_ = 0;
+	sum_ = Eigen::Vector3d(0, 0, 0);
+	xx_ = 0;
+	xy_ = 0;
+	xz_ = 0;
+	yy_ = 0;
+	yz_ = 0;
+	zz_ = 0;
 }
 
-void PrincipalPlaneAccumulator::clear()
-{
+void PrincipalPlaneAccumulator::addPoint(Eigen::Vector4f point) {
 
-    count=0;
-    sum = Eigen::Vector3d(0,0,0);//(0,0,0);
-    xx = 0;
-    xy = 0;
-    xz = 0;
-    yy = 0;
-    yz = 0;
-    zz = 0;
+	Eigen::Vector3d p = Eigen::Vector3d(point[0], point[1], point[2]);
+	sum_ += p;
+	xx_ += p[0] * p[0];
+	xy_ += p[0] * p[1];
+	xz_ += p[0] * p[2];
+	yy_ += p[1] * p[1];
+	yz_ += p[1] * p[2];
+	zz_ += p[2] * p[2];
+	count_++;
 }
 
-void PrincipalPlaneAccumulator::addPoint(Eigen::Vector4f point)
-{
-    Eigen::Vector3d p = Eigen::Vector3d(point[0],point[1],point[2]);
-    sum += p;
-    xx += p[0]*p[0];
-    xy += p[0]*p[1];
-    xz += p[0]*p[2];
-    yy += p[1]*p[1];
-    yz += p[1]*p[2];
-    zz += p[2]*p[2];
-    count++;
+Eigen::Vector4f PrincipalPlaneAccumulator::plane() {
+	Matrix3d mat;
+	mat << xx_, xy_, xz_,
+	       xy_, yy_, yz_,
+	       xz_, yz_, zz_;
 
+	Vector3d plane = mat.ldlt().solve(sum_);
+
+	return Vector4f(plane[0], plane[1], plane[2], 0);
 }
 
-Eigen::Vector4f PrincipalPlaneAccumulator::plane()
-{
-    Matrix3d mat;
-    mat << xx, xy, xz,
-            xy, yy, yz,
-            xz, yz, zz;
-
-    Vector3d plane = mat.ldlt().solve(sum);
-    return Vector4f(plane[0],plane[1],plane[2],0);
-}
-
-
-Vector4f PrincipalPlaneAccumulator::centerPoint()
-{
-    Vector3d c = sum*(1.0/double(count));
-    return Vector4f(c[0],c[1],c[2],0.0f);
+Vector4f PrincipalPlaneAccumulator::centerPoint() {
+	Vector3d c = sum_ * (1.0 / double(count_));
+	return Vector4f(c[0], c[1], c[2], 0.0f);
 }
 

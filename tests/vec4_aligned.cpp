2e4a8f8de81ab35dc12c47351161810cae19fa82
Simon <simon.schreiberhuber@gmx.net>
2019-11-14 17:17:53 +0100

    feat: Some more tests for alignment on different machines.

--- tests/vec4_aligned.cpp
+++ tests/vec4_aligned.cpp
@@ -1,20 +1,28 @@
 //
 // Created by simon on 14.11.19.
 //
-
+#include <iostream>
 #include <vector>
 #include <Eigen/Eigen>
 using namespace std;
 using namespace Eigen;
-
+struct Test{
+	float bla;
+	Vector4f blabla;
+};
 
 int main(int argc,char *varg[]){
 	int size = 100;
-	for(int i=0;i<10;i++){
-		vector<Vector4f,aligned_allocator<Vector4f>> test(size);
+	Test *t = new Test();
+	cout << t << endl;
+	cout << &t->blabla << endl;
+	cout << "dist " << (unsigned long)&t->blabla - (unsigned long)t << endl;
 
+	for(int i=0;i<10;i++){
+		vector<Vector4f,aligned_allocator<Vector4f>> *test = new vector<Vector4f,aligned_allocator<Vector4f>>(size);
+		cout << &test[0] << endl;
 		for(int j=0;j<size;j++){
-			test[j] = Vector4f(1,1,1,1);
+			(*test)[j] = Vector4f(1,1,1,1);
 		}
 	}
 	return 0;
--- tests/vec4_cuda.cu
+++ tests/vec4_cuda.cu
@@ -1,21 +1,30 @@
 //
 // Created by simon on 14.11.19.
 //
-
+#include <iostream>
 #include <vector>
 #include <Eigen/Eigen>
 using namespace std;
 using namespace Eigen;
-
+struct Test{
+	float bla;
+	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+	Vector4f blabla;
+};
 
 int main(int argc,char *varg[]){
 	int size = 100;
+	Test *t = new Test();
+	cout << t << endl;
+	cout << &t->blabla << endl;
+	cout << "dist " << (unsigned long)&t->blabla - (unsigned long)t << endl;
+	t.blabla = Vector4(1,1,1,1);
 	for(int i=0;i<10;i++){
-		vector<Vector4f> test(size);
-
+		vector<Vector4f> *test = new vector<Vector4f>(size);
+		cout << &test[0] << endl;
 		for(int j=0;j<size;j++){
-			test[j] = Vector4f(1,1,1,1);
+			(*test)[j] = Vector4f(1,1,1,1);
 		}
 	}
-    return 0;
+	return 0;
 }

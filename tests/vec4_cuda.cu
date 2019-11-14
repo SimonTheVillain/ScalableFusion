211bb6273615bdb101de0a16a12dc12d18d291bb
Simon <simon.schreiberhuber@gmx.net>
2019-11-14 16:44:53 +0100

    feat: Test various cases of aligned access for eigen.

---
+++ tests/vec4_cuda.cu
@@ -0,0 +1,21 @@
+//
+// Created by simon on 14.11.19.
+//
+
+#include <vector>
+#include <Eigen/Eigen>
+using namespace std;
+using namespace Eigen;
+
+
+int main(int argc,char *varg[]){
+	int size = 100;
+	for(int i=0;i<10;i++){
+		vector<Vector4f> test(size);
+
+		for(int j=0;j<size;j++){
+			test[j] = Vector4f(1,1,1,1);
+		}
+	}
+    return 0;
+}

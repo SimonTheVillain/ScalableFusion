7fda3095c9e66a4821d20e068e2ea672a4727787
Nikolaus Wagner <nikolaus.wagner@outlook.com>
2019-12-13 15:19:21 +0100

    feat: Add logging

---
+++ logging/test/test_logging.cpp
@@ -0,0 +1,22 @@
+
+#define ENABLE_LOGGING
+#define LOGFILE "./log.txt"
+#include <logger.h>
+
+int main(void) {
+	START_LOGGING(logging::Level::ERROR);
+
+	LOG(logging::Level::DEBUG, "This is a debug message.");
+	LOG(logging::Level::ERROR, "This is an error message.");
+
+	STOP_LOGGING();
+	
+	START_LOGGING(logging::Level::DEBUG);
+
+	LOG(logging::Level::DEBUG, "This is a debug message.");
+	LOG(logging::Level::ERROR, "This is an error message.");
+
+	STOP_LOGGING();
+
+	return 0;
+}
